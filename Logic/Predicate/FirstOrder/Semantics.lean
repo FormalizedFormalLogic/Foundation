1ce47da9f2226c129cc283fcd0898102d9144068
jeh <hodellurt@gmail.com>
2023-02-11 20:32:40 +0900

    feat: Semantics

---
+++ Logic/Predicate/FirstOrder/Semantics.lean
@@ -0,0 +1,87 @@
+import Logic.Predicate.FirstOrder.Language
+import Logic.Predicate.Semantics
+
+variable {L : Language} {μ μ₁ μ₂ μ₃ : Type _}
+
+namespace FirstOrder
+
+namespace SubFormula
+variable {n n₁ n₂ n₃ : ℕ} (S : Structure₁ L) (e : Fin n → S) (Φ : μ → S)
+
+def subval' (Φ : μ → S) : ∀ {n}, (Fin n → S) → SubFormula L μ n → Prop
+  | _, _, ⊤        => True
+  | _, _, ⊥        => False
+  | _, e, rel p v  => S.rel p (fun i => SubTerm.val S e Φ (v i))
+  | _, e, nrel p v => ¬S.rel p (fun i => SubTerm.val S e Φ (v i))
+  | _, e, p ⋏ q    => p.subval' Φ e ∧ q.subval' Φ e
+  | _, e, p ⋎ q    => p.subval' Φ e ∨ q.subval' Φ e
+  | _, e, ∀' p     => ∀ x : S, (p.subval' Φ (x :> e))
+  | _, e, ∃' p     => ∃ x : S, (p.subval' Φ (x :> e))
+
+@[simp] lemma subval'_neg (p : SubFormula L μ n) :
+    subval' S Φ e (¬'p) = ¬subval' S Φ e p :=
+  by induction p using rec' <;> simp[*, subval', ←neg_eq, or_iff_not_imp_left]
+
+def subval (e : Fin n → S) (Φ : μ → S) : SubFormula L μ n →L Prop where
+  toFun := subval' S Φ e
+  map_top' := rfl
+  map_bot' := rfl
+  map_and' := by simp[subval']
+  map_or' := by simp[subval']
+  map_neg' := by simp[subval'_neg]
+  map_imp' := by simp[imp_eq, subval'_neg, ←neg_eq, subval', imp_iff_not_or]
+
+def val (Φ : μ → S) : Formula L μ →L Prop := subval S Fin.nil Φ
+
+notation:50 S " ⊧₁[" e :80 "] "p :0 => val S e p
+
+def models (S : Structure₁ L) (p : Formula L μ) : Prop := ∀ e, S ⊧₁[e] p
+
+
+@[simp] lemma subval_rel {k} {r : L.rel k} {v} :
+    subval S e Φ (rel r v) ↔ S.rel r (fun i => SubTerm.val S e Φ (v i)) := by simp[subval, subval']
+
+@[simp] lemma subval_nrel {k} {r : L.rel k} {v} :
+    subval S e Φ (nrel r v) ↔ ¬S.rel r (fun i => SubTerm.val S e Φ (v i)) := by simp[subval, subval']
+
+@[simp] lemma subval_all {p : SubFormula L μ (n + 1)} :
+    subval S e Φ (∀' p) ↔ ∀ x : S, subval S (x :> e) Φ p := by simp[subval, subval']
+
+@[simp] lemma subval_ex {p : SubFormula L μ (n + 1)} :
+    subval S e Φ (∃' p) ↔ ∃ x : S, subval S (x :> e) Φ p := by simp[subval, subval']
+
+lemma subval_bind (fixed : Fin n₁ → SubTerm L μ₂ n₂) (free : μ₁ → SubTerm L μ₂ n₂) (e : Fin n₂ → S) (Φ : μ₂ → S) (p : SubFormula L μ₁ n₁) :
+    subval S e Φ (bind fixed free p) = subval S (fun x => SubTerm.val S e Φ (fixed x)) (fun x => SubTerm.val S e Φ (free x)) p :=
+  by
+  induction p using rec' generalizing n₂ <;> simp[*, SubTerm.val_bind]
+  · apply forall_congr'; intros a; apply of_eq; congr; exact funext $ Fin.cases (by simp) (by simp)
+  · apply exists_congr; intros a; apply of_eq; congr; exact funext $ Fin.cases (by simp) (by simp)
+
+lemma subval_map (fixed : Fin n₁ → Fin n₂) (free : μ₁ → μ₂) (e : Fin n₂ → S) (Φ : μ₂ → S) (p : SubFormula L μ₁ n₁) :
+    subval S e Φ (map fixed free p) = subval S (e ∘ fixed) (Φ ∘ free) p :=
+  by simp[map, subval_bind, Function.comp]
+
+lemma subval_subst (u : SubTerm L μ n) (p : SubFormula L μ (n + 1)) :
+    subval S e Φ (subst u p) = subval S (e <: u.val S e Φ) Φ p :=
+  by simp[subst, subval_bind]; apply of_eq; congr ; exact funext $ Fin.lastCases (by simp) (by simp)
+
+section Syntactic
+variable (Ψ : ℕ → S)
+
+end Syntactic
+
+end SubFormula
+
+instance : HasDoubleTurnstile (Structure₁ L) (Formula L μ) := ⟨SubFormula.models⟩
+
+instance : HasDoubleTurnstile (Theory L μ) (Formula L μ) :=
+  ⟨fun T p => ∀ S : Structure₁ L, (∀ q ∈ T, S ⊧ q) → S ⊧ p⟩
+
+lemma models_def {S : Structure₁ L} {p : Formula L μ} : S ⊧ p ↔ (∀ e, S ⊧₁[e] p) :=
+  by simp[HasDoubleTurnstile.doubleTurnstile, SubFormula.models]
+
+lemma semanticConsequence_def {T : Theory L μ} {p : Formula L μ} :
+    T ⊧ p ↔ (∀ S : Structure₁ L, (∀ q ∈ T, S ⊧ q) → S ⊧ p) :=
+  by simp[HasDoubleTurnstile.doubleTurnstile, SubFormula.models]
+
+end FirstOrder
---
+++ Logic/Predicate/Semantics.lean
@@ -0,0 +1,55 @@
+import Logic.Predicate.Term
+
+variable {L : Language} {μ μ₁ μ₂ : Type _}
+
+structure Structure₁ (L : Language.{u}) :=
+(dom : Type u)
+(domInhabited : Inhabited dom)
+(func : {k : ℕ} → L.func k → (Fin k → dom) → dom)
+(rel : {k : ℕ} → L.rel  k → (Fin k → dom) → Prop)
+
+instance Structure₁_coe : CoeSort (Structure₁ L) (Type _) := ⟨Structure₁.dom⟩
+
+instance Structure₁_Inhabited (S : Structure₁ L) : Inhabited S := S.domInhabited
+
+namespace SubTerm
+variable {n n₁ n₂ : ℕ} (S : Structure₁ L) (e : Fin n → S) (Φ : μ → S)
+
+@[simp] def val : SubTerm L μ n → S
+  | (#x)       => e x
+  | (&x)       => Φ x
+  | (func f v) => S.func f (fun i => (v i).val)
+
+lemma val_bind (fixed : Fin n₁ → SubTerm L μ₂ n₂) (free : μ₁ → SubTerm L μ₂ n₂) (e : Fin n₂ → S) (Φ : μ₂ → S) (t : SubTerm L μ₁ n₁) :
+    (bind fixed free t).val S e Φ = t.val S (fun x => val S e Φ (fixed x)) (fun x => val S e Φ (free x)) :=
+  by induction t <;> simp[*]
+
+lemma val_map (fixed : Fin n₁ → Fin n₂) (free : μ₁ → μ₂) (e : Fin n₂ → S) (Φ : μ₂ → S) (t : SubTerm L μ₁ n₁) :
+    (map fixed free t).val S e Φ = t.val S (fun x => e (fixed x)) (fun x => Φ (free x)) := val_bind _ _ _ _ _ _
+
+lemma val_subst (u : SubTerm L μ n) (t : SubTerm L μ (n + 1)) :
+    (subst u t).val S e Φ = t.val S (e <: u.val S e Φ) Φ :=
+  by simp[subst, val_bind]; congr; exact funext $ Fin.lastCases (by simp) (by simp)
+
+@[simp] lemma val_fixedSucc (a : S) (t : SubTerm L μ n) :
+    t.fixedSucc.val S (a :> e) Φ = t.val S e Φ := by simp[fixedSucc, val_map]
+
+section Syntactic
+variable (Ψ : ℕ → S)
+
+lemma val_shift (t : SyntacticSubTerm L n) :
+    t.shift.val S e Ψ = t.val S e (fun x => Ψ $ x + 1) := by simp[shift, val_map]
+
+lemma val_push (a : S) (t : SyntacticSubTerm L (n + 1)) :
+    t.push.val S e (a :>ₙ Ψ) = t.val S (e <: a) Ψ :=
+  by simp[push, val_bind]; congr; exact funext $ Fin.lastCases (by simp) (by simp)
+
+lemma val_pull (a : S) (t : SyntacticSubTerm L n) :
+    t.pull.val S (e <: a) Ψ = t.val S e (a :>ₙ Ψ) :=
+  by simp[pull, val_bind]; congr; exact funext (Nat.cases (by simp) (by simp))
+
+end Syntactic
+
+end SubTerm
+
+
