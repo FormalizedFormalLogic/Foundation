7f7d161f894c7cb70a964d1e1477614be9157fe8
jeh <hodellurt@gmail.com>
2023-09-04 05:12:43 +0900

    fix(Main)

--- Main.lean
+++ Main.lean
@@ -1,9 +1,1 @@
 import «Logic»
-
-open FirstOrder
-
-def d : FirstOrder.Derivation₁ (L := (Language.relational (fun _ => ℕ)))
-    “(([Language.toRelational 0]() → [Language.toRelational 1]()) → [Language.toRelational 0]()) → [Language.toRelational 0]()” := by proveDerivation₁
-
-unsafe def main : IO Unit :=
-  IO.println s!"{repr d}"
