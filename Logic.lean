bc98adb9333b8d031ad18670b03bdda24c744df3
jeh <hodellurt@gmail.com>
2023-09-04 05:10:03 +0900

    fix

--- Logic.lean
+++ Logic.lean
@@ -1,19 +1,45 @@
 import Logic.Vorspiel.Vorspiel
+import Logic.Vorspiel.Order
 import Logic.Vorspiel.Meta
-import Logic.Vorspiel.Logic
-import Logic.Predicate.Language
-import Logic.Predicate.Term
-import Logic.Predicate.Semantics
-import Logic.Predicate.Coding
-import Logic.Predicate.Meta
-import Logic.Predicate.FirstOrder.Formula
-import Logic.Predicate.FirstOrder.Language
-import Logic.Predicate.FirstOrder.Calculus
-import Logic.Predicate.FirstOrder.Coding
-import Logic.Predicate.FirstOrder.Semantics
-import Logic.Predicate.FirstOrder.Ultraproduct
-import Logic.Predicate.FirstOrder.Eq
-import Logic.Predicate.FirstOrder.Meta
-import Logic.Predicate.FirstOrder.ProofAutomation
-import Logic.Predicate.FirstOrder.Completeness.Completeness
+
+import Logic.Logic.LogicSymbol
+import Logic.Logic.Logic
+
+-- Propositional
+
+import Logic.Propositional.Basic.Formula
+
+-- FirstOrder
+
+import Logic.Predicate.FirstOrder.Basic.Language
+import Logic.Predicate.FirstOrder.Basic.Term.Term
+import Logic.Predicate.FirstOrder.Basic.Term.Coding
+import Logic.Predicate.FirstOrder.Basic.Term.Elab
+import Logic.Predicate.FirstOrder.Basic.Term.Meta
+import Logic.Predicate.FirstOrder.Basic.Formula.Formula
+import Logic.Predicate.FirstOrder.Basic.Formula.Coding
+import Logic.Predicate.FirstOrder.Basic.Formula.Elab
+import Logic.Predicate.FirstOrder.Basic.Formula.Meta
+import Logic.Predicate.FirstOrder.Basic.Semantics
+import Logic.Predicate.FirstOrder.Basic.Calculus
+import Logic.Predicate.FirstOrder.Basic.Eq
+import Logic.Predicate.FirstOrder.Basic.Soundness
+
 import Logic.Predicate.FirstOrder.Hauptsatz
+import Logic.Predicate.FirstOrder.Ultraproduct
+
+import Logic.Predicate.FirstOrder.Completeness.Coding
+import Logic.Predicate.FirstOrder.Completeness.SubLanguage
+import Logic.Predicate.FirstOrder.Completeness.SearchTree
+import Logic.Predicate.FirstOrder.Completeness.Completeness
+
+import Logic.Predicate.FirstOrder.Principia.Principia
+import Logic.Predicate.FirstOrder.Principia.RewriteFormula
+import Logic.Predicate.FirstOrder.Principia.Meta
+
+import Logic.Predicate.FirstOrder.Order.Le
+
+import Logic.Predicate.FirstOrder.Arith.Hierarchy
+import Logic.Predicate.FirstOrder.Arith.Theory
+import Logic.Predicate.FirstOrder.Arith.Model
+import Logic.Predicate.FirstOrder.Arith.Robinson
