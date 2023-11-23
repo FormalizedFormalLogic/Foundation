ad5f85ff7c8388f8fedcbf05dbbe560ea76c5266
jeh <hodellurt@gmail.com>
2023-11-24 00:39:25 +0900

    feat: Principia

--- Logic/Logic/Principia/Principia.lean
+++ Logic/Logic/Principia/Principia.lean
@@ -4,22 +4,38 @@ namespace LO
 
 namespace Logic
 
-class Principia (F : Type u) [LogicSymbol F] extends System F where
-  Pr : Set F → List F → F → Type u
-  toBew : {T : Set F} → {Δ : List F} → {f : F} → Pr T Δ f → T ⊢ Δ.conj ⟶ f
-  PrWeakening' : {Δ Γ : List F} → {f : F} → Δ ⊆ Γ → Pr T Δ f → Pr T Γ f
+variable {F : Type u} [LogicSymbol F]
+
+class Axiom (F : Type u) (α : outParam (Type*)) where
+  toTheory : α → Set F
+
+class Principia (F : Type u) [LogicSymbol F] (α : outParam (Type*)) extends Axiom F α where
+  Pr : α → List F → F → Type u
+  byAxiom (a : α) : σ ∈ @Axiom.toTheory F _ _ a → Pr a Δ σ
+
+class LawfulPrincipia (F : Type u) [LogicSymbol F] [System F] (α : outParam (Type*)) [Principia F α] where
+  toBew {a : α} {f : F} : Principia.Pr a [] f → Axiom.toTheory a ⊢ f
 
 namespace Principia
 
-notation Δ:0 " ⟹[" T "] " f => Pr T Δ f
+local notation Δ:0 " ⟹[" T "] " f => Pr T Δ f
 
-class Propositional (F : Type u) [LogicSymbol F] extends Principia F where
-  verumRight {T : Set F} {Δ} : Δ ⟹[T] ⊤
-  andRight {T : Set F} {Δ f₁ f₂} : (Δ ⟹[T] f₁) → (Δ ⟹[T] f₂) → (Δ ⟹[T] f₁ ⋏ f₂)
-  orRight₁ {T : Set F} {Δ f₁ f₂} : (Δ ⟹[T] f₁) → (Δ ⟹[T] f₁ ⋎ f₂)
-  orRight₂ {T : Set F} {Δ f₁ f₂} : (Δ ⟹[T] f₂) → (Δ ⟹[T] f₁ ⋎ f₂)
-  intro {T : Set F} {Δ f₁ f₂} : ((f₁ :: Δ) ⟹[T] f₂) → (Δ ⟹[T] f₁ ⟶ f₂)
-  absurd {T : Set F} {Δ f} : ((f :: Δ) ⟹[T] ⊥) → (Δ ⟹[T] ~f)
+class Intuitionistic (F : Type u) [LogicSymbol F] (α : outParam (Type*)) extends Principia F α where
+  weakening' {α : α} {Δ Γ} {p q : F}  : ~p :: Δ ⊆ ~q :: Γ → (Δ ⟹[a] p) → (Γ ⟹[a] q)
+  trans {a : α} {Δ} {p q : F}         : (Δ ⟹[a] p) → ((p :: Δ) ⟹[a] q) → (Δ ⟹[a] q)
+  assumption {a : α} {Δ} {p : F}      : p ∈ Δ → (Δ ⟹[a] p)
+  contradiction {a : α} {Δ} {p q : F} : (Δ ⟹[a] p) → (Δ ⟹[a] ~p) → (Δ ⟹[a] q)
+  trivial {a : α} {Δ : List F}        : Δ ⟹[a] ⊤
+  explode {a : α} {Δ} {p : F}         : (Δ ⟹[a] ⊥) → Δ ⟹[a] p
+  intro {a : α} {Δ} {p q : F}         : ((p :: Δ) ⟹[a] q) → (Δ ⟹[a] p ⟶ q)
+  modusPonens {a : α} {Δ} {p q : F}   : (Δ ⟹[a] p ⟶ q) → (Δ ⟹[a] p) → (Δ ⟹[a] q)
+  split {a : α} {Δ} {p q : F}         : (Δ ⟹[a] p) → (Δ ⟹[a] q) → (Δ ⟹[a] p ⋏ q)
+  andLeft {a : α} {Δ} {p q : F}       : (Δ ⟹[a] p ⋏ q) → (Δ ⟹[a] p)
+  andRight {a : α} {Δ} {p q : F}      : (Δ ⟹[a] p ⋏ q) → (Δ ⟹[a] q)
+  orLeft {a : α} {Δ} {p q : F}        : (Δ ⟹[a] p) → (Δ ⟹[a] p ⋎ q)
+  orRight {a : α} {Δ} {p q : F}       : (Δ ⟹[a] q) → (Δ ⟹[a] p ⋎ q)
+  cases {a : α} {Δ} {p q r : F}       :
+    (Δ ⟹[a] (p ⋎ q)) → ((p :: Δ) ⟹[a] r) → ((q :: Δ) ⟹[a] r) → (Δ ⟹[a] r)
 
 variable {F : Type u} [LogicSymbol F]
 
