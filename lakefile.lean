44d916f541c5fd321122299c0d1bfb413d60d11b
palalansouki <palalansouki@gmail.com>
2023-12-23 23:19:43 +0900

    add documentation (wip)

--- lakefile.lean
+++ lakefile.lean
@@ -16,3 +16,6 @@ require mathlib from git
 lean_exe «logic» {
   root := `Main
 }
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
