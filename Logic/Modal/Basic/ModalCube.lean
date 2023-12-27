da09e3ec33836ccadd93b6b0b4311bad547f4f37
SnO2WMaN <me@sno2wman.net>
2023-12-27 10:57:55 +0900

    add modal cube

---
+++ Logic/Modal/Basic/ModalCube.lean
@@ -0,0 +1,25 @@
+import Logic.Modal.Basic.HilbertStyle
+
+namespace LO.Modal.Hilbert
+
+section S5Equivalence
+
+variable (F : Type u) [ModalLogicSymbol F] [TwoSided F]
+
+class LogicKTB5 extends LogicK F, HasAxiomT F, HasAxiom5 F
+
+class LogicKT45 extends LogicK F, HasAxiomT F, HasAxiom4 F, HasAxiom5 F
+
+class LogicKT4B5 extends LogicK F, HasAxiomT F, HasAxiom4 F, HasAxiomB F, HasAxiom5 F
+
+class LogicKDB5 extends LogicK F, HasAxiomD F, HasAxiomB F, HasAxiom5 F
+
+class LogicKD4B5 extends LogicK F, HasAxiomD F, HasAxiom4 F, HasAxiomB F, HasAxiom5 F
+
+class LogicKT4B extends LogicK F, HasAxiomT F, HasAxiom4 F, HasAxiomB F
+
+class LogicKD4B extends LogicK F, HasAxiomD F, HasAxiom4 F, HasAxiomB F
+
+end S5Equivalence
+
+end LO.Modal.Hilbert
