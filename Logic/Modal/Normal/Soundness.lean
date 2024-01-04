0da2439a8da50f63b19d15f4317200e81d497f13
SnO2WMaN <me@sno2wman.net>
2024-01-05 02:33:29 +0900

    rename theorem name

--- Logic/Modal/Normal/Soundness.lean
+++ Logic/Modal/Normal/Soundness.lean
@@ -13,7 +13,7 @@ variable (Λ : Logic (Formula α)) [hΛ : LogicDefines β Λ]
   TODO: より一般にこの形で証明できる事実ではないだろうか？
   [LogicK.Hilbert Bew] (Γ : Set (Formula α)) (hΓ : Γ = ∅) (p : Formula α) (f : Frame β) (d : Bew Γ p) : (Γ ⊨ᴹᶠ[f] p)
 -/
-lemma Logic.sounds
+lemma Logic.Hilbert.sounds
   (Λ : Logic (Formula α)) [hΛ : LogicDefines β Λ]
   (p : Formula α)
   (f : Frame β) (hf : LogicDefines.definability Λ f.rel)
@@ -37,7 +37,7 @@ lemma Logic.sounds
     exact hw rww' (hΓ q hq);
   -/
 
-lemma Logic.consistent
+lemma Logic.Hilbert.consistent
   (β) [Inhabited β]
   (Λ : Logic (Formula α)) [hΛ : LogicDefines β Λ]
   : (⊬ᴹ(Λ)! (⊥ : Formula α)) := by
@@ -46,18 +46,18 @@ lemma Logic.consistent
     let ⟨f, hf⟩ := h;
     exact Frames.bot_def hf;
   have ⟨tf, htf⟩ := hΛ.trivial_frame;
-  existsi tf; exact Logic.sounds Λ ⊥ tf htf hC.some;
+  existsi tf; exact Logic.Hilbert.sounds Λ ⊥ tf htf hC.some;
 
-theorem LogicK.sounds {p : Formula α} (f : Frame β) (hf : (@LogicK.defines β α).definability f.rel)
+theorem LogicK.Hilbert.sounds {p : Formula α} (f : Frame β) (hf : (@LogicK.defines β α).definability f.rel)
   : (⊢ᴹ(𝐊) p) → (⊧ᴹᶠ[f] p) := by
-  exact Logic.sounds 𝐊 p f hf;
+  exact Logic.Hilbert.sounds 𝐊 p f hf;
 
-theorem LogicK.consistency : ⊬ᴹ(𝐊)! (⊥ : Formula α) := Logic.consistent β 𝐊
+theorem LogicK.Hilbert.consistency : ⊬ᴹ(𝐊)! (⊥ : Formula α) := Logic.Hilbert.consistent β 𝐊
 
-theorem LogicKD.sounds {p : Formula α} (f : Frame β) (hf : (@LogicKD.defines β α).definability f.rel)
+theorem LogicKD.Hilbert.sounds {p : Formula α} (f : Frame β) (hf : (@LogicKD.defines β α).definability f.rel)
   (h : ⊢ᴹ(𝐊𝐃) p) : (⊧ᴹᶠ[f] p) := by
-  exact Logic.sounds 𝐊𝐃 p f hf h;
+  exact Logic.Hilbert.sounds 𝐊𝐃 p f hf h;
 
-theorem LogicKD.consistency : ⊬ᴹ(𝐊𝐃)! (⊥ : Formula α) := Logic.consistent β 𝐊𝐃
+theorem LogicKD.Hilbert.consistency : ⊬ᴹ(𝐊𝐃)! (⊥ : Formula α) := Logic.Hilbert.consistent β 𝐊𝐃
 
 end LO.Modal.Normal
