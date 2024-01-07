c3758b28b117bcf8b7c854c4040848a2f7acbece
palalansouki <palalansouki@gmail.com>
2024-01-08 03:53:05 +0900

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,16 @@
+import Lake
+open Lake DSL
+
+package «arithmetization» {
+  -- add any package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+require logic from git "https://github.com/iehality/lean4-logic"
+
+@[default_target]
+lean_lib «Arithmetization» {
+  -- add any library configuration options here
+}
