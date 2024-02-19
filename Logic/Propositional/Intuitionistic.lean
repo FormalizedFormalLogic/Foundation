7fe68dbce68a1182050704e1c2424cae06f57862
SnO2WMaN <me@sno2wman.net>
2024-02-19 14:02:18 +0900

    feat(Int Prop): No LEM

--- Logic/Propositional/Intuitionistic.lean
+++ Logic/Propositional/Intuitionistic.lean
@@ -4,3 +4,4 @@ import Logic.Propositional.Intuitionistic.Deduction
 import Logic.Propositional.Intuitionistic.Kripke.Semantics
 import Logic.Propositional.Intuitionistic.Kripke.Soundness
 import Logic.Propositional.Intuitionistic.Kripke.Completeness
+import Logic.Propositional.Intuitionistic.Kripke.LEM
