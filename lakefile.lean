40d35be75d75861595a5e3d5aeb5406c627421a2
palalansouki <palalansouki@gmail.com>
2024-03-26 02:16:02 +0900

    add: docs

--- lakefile.lean
+++ lakefile.lean
@@ -7,6 +7,9 @@ package «arithmetization» {
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4.git"
 
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "780bbec107cba79d18ec55ac2be3907a77f27f98"
+
 require logic from git "https://github.com/iehality/lean4-logic"
 
 @[default_target]
