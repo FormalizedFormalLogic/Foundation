b087cac73debba34acb3b8189d8d542cfa97605b
SnO2WMaN <me@sno2wman.net>
2024-04-15 13:06:58 +0900

    notation fix

--- Logic/Propositional/Intuitionistic/Kripke/Extended.lean
+++ Logic/Propositional/Intuitionistic/Kripke/Extended.lean
@@ -7,37 +7,37 @@ variable {α β}
 
 @[simp]
 def Formula.ExtendedKripkeSatisfies (P : Kripke.Frame α → Prop) (M : Kripke.Model α β) (w : α) (p : Formula β) : Prop := (P M.frame) → (p.KripkeSatisfies M w)
-notation w " ⊩ⁱ[" P "," M "] " p => Formula.ExtendedKripkeSatisfies P M w p
+notation w " ⊩ᴾ[" P "," M "] " p => Formula.ExtendedKripkeSatisfies P M w p
 
-@[simp] lemma Formula.ExtendedKripkeSatisfies.iff_int {p : Formula β} : (w ⊩ⁱ[λ _ => True, M] p) ↔ (w ⊩ⁱ[M] p) := by simp;
-
-
-@[simp]
-def Theory.ExtendedKripkeSatisfies (P : Kripke.Frame α → Prop) (M : Kripke.Model α β) (w : α) (Γ : Theory β) := ∀ p ∈ Γ, (w ⊩ⁱ[P, M] p)
-notation w " ⊩ⁱ[" P "," M "] " Γ => Theory.ExtendedKripkeSatisfies P M w Γ
-
-lemma Theory.ExtendedKripkeSatisfies.iff_int {Γ : Theory β} : (w ⊩ⁱ[λ _ => True, M] Γ) ↔ (w ⊩ⁱ[M] Γ) := by simp; rfl;
+@[simp] lemma Formula.ExtendedKripkeSatisfies.iff_int {p : Formula β} : (w ⊩ᴾ[λ _ => True, M] p) ↔ (w ⊩ⁱ[M] p) := by simp;
 
 
 @[simp]
-def Formula.ExtendedKripkeModels (P : Kripke.Frame α → Prop) (M : Kripke.Model α β) (p : Formula β) : Prop := ∀ w, (w ⊩ⁱ[P, M] p)
-notation M " ⊧[" P "] " p => Formula.ExtendedKripkeModels P M p
+def Theory.ExtendedKripkeSatisfies (P : Kripke.Frame α → Prop) (M : Kripke.Model α β) (w : α) (Γ : Theory β) := ∀ p ∈ Γ, (w ⊩ᴾ[P, M] p)
+notation w " ⊩ᴾ[" P "," M "] " Γ => Theory.ExtendedKripkeSatisfies P M w Γ
 
-@[simp] lemma Formula.ExtendedKripkeModels.iff_int {p : Formula β} : (M ⊧[λ _ => True] p) ↔ (M ⊧ⁱ p) := by simp; rfl;
-
-
-@[simp]
-def Formula.ExtendedKripkeValid (P : ∀ {α : Type}, Kripke.Frame α → Prop) (p : Formula β) : Prop := ∀ {α}, ∀ (M : Kripke.Model α β), (M ⊧[P] p)
-notation "⊧[" P "] " p => Formula.ExtendedKripkeValid P p
-
-@[simp] lemma Formula.ExtendedKripkeValid.iff_int {p : Formula β} : (⊧[λ _ => True] p) ↔ (⊧ⁱ p) := by simp; rfl;
+lemma Theory.ExtendedKripkeSatisfies.iff_int {Γ : Theory β} : (w ⊩ᴾ[λ _ => True, M] Γ) ↔ (w ⊩ⁱ[M] Γ) := by simp; rfl;
 
 
 @[simp]
-def Formula.ExtendedKripkeConsequence (P : ∀ {α : Type}, Kripke.Frame α → Prop) (Γ : Theory β) (p : Formula β) : Prop := ∀ {α : Type}, ∀ (M : Kripke.Model α β) w, (w ⊩ⁱ[(@P α), M] Γ) → (w ⊩ⁱ[(@P α), M] p)
-notation:50 Γ " ⊨ⁱ[" P "] " p => Formula.ExtendedKripkeConsequence P Γ p
+def Formula.ExtendedKripkeModels (P : Kripke.Frame α → Prop) (M : Kripke.Model α β) (p : Formula β) : Prop := ∀ w, (w ⊩ᴾ[P, M] p)
+notation M " ⊧ᴾ[" P "] " p => Formula.ExtendedKripkeModels P M p
 
-@[simp] lemma Formula.ExtendedKripkeConsequence.iff_int : (Formula.ExtendedKripkeConsequence (λ _ => True) Γ p) ↔ (Formula.KripkeConsequence Γ p) := by simp; rfl;
+@[simp] lemma Formula.ExtendedKripkeModels.iff_int {p : Formula β} : (M ⊧ᴾ[λ _ => True] p) ↔ (M ⊧ⁱ p) := by simp; rfl;
+
+
+@[simp]
+def Formula.ExtendedKripkeValid (P : ∀ {α : Type}, Kripke.Frame α → Prop) (p : Formula β) : Prop := ∀ {α}, ∀ (M : Kripke.Model α β), (M ⊧ᴾ[P] p)
+notation "⊧ᴾ[" P "] " p => Formula.ExtendedKripkeValid P p
+
+@[simp] lemma Formula.ExtendedKripkeValid.iff_int {p : Formula β} : (⊧ᴾ[λ _ => True] p) ↔ (⊧ⁱ p) := by simp; rfl;
+
+
+@[simp]
+def Formula.ExtendedKripkeConsequence (P : ∀ {α : Type}, Kripke.Frame α → Prop) (Γ : Theory β) (p : Formula β) : Prop := ∀ {α : Type}, ∀ (M : Kripke.Model α β) w, (w ⊩ᴾ[(@P α), M] Γ) → (w ⊩ᴾ[(@P α), M] p)
+notation:50 Γ " ⊨ᴾ[" P "] " p => Formula.ExtendedKripkeConsequence P Γ p
+
+@[simp] lemma Formula.ExtendedKripkeConsequence.iff_int : (Γ ⊨ᴾ[(λ _ => True)] p) ↔ (Formula.KripkeConsequence Γ p) := by simp; rfl;
 
 
 section LEM
@@ -53,7 +53,7 @@ lemma _root_.full_of_refl_antisymm_eucl {rel : α → α → Prop} (hRefl : Refl
     exact hAntisymm hxy hyx;
   . intro exy; simp_all; apply hRefl;
 
-example : ⊧[Full] (p ⋎ ~p) := by
+example : ⊧ᴾ[Full] (p ⋎ ~p) := by
   intro _ M w hf;
   by_cases h : w ⊩ⁱ[M] p;
   case pos => left; assumption;
