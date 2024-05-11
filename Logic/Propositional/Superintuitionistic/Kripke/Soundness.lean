aa3eb9629489a0931fd39c0a55f78b97202a81eb
SnO2WMaN <me@sno2wman.net>
2024-05-12 03:44:15 +0900

    fx

--- Logic/Propositional/Superintuitionistic/Kripke/Soundness.lean
+++ Logic/Propositional/Superintuitionistic/Kripke/Soundness.lean
@@ -6,20 +6,20 @@ namespace LO.Propositional.Superintuitionistic.Kripke
 variable {W α : Type*}
 
 open Formula.Kripke.ValidOnFrameClass
+open FrameClass
 
 -- TODO: Expand 𝐄𝐅𝐐-𝐈𝐧𝐭
-
 lemma sound (d : 𝐄𝐅𝐐 ⊢ p) : (𝐈𝐧𝐭 W α) ⊧ p := by
   induction d with
   | eaxm h =>
     obtain ⟨q, hq⟩ := by simpa [axiomEFQ] using h;
     subst hq;
     simp;
-  | mdp _ _ ihpq ihp => exact mdp (by simp_all) ihpq ihp;
-  | conj₃ => apply conj₃; simp_all;
-  | disj₃ => apply disj₃; simp_all;
-  | imply₁ => apply imply₁; simp_all;
-  | imply₂ => apply imply₂; simp_all; simp_all;
+  | mdp _ _ ihpq ihp => exact mdp (by simp_all [Intuitionistic]) ihpq ihp;
+  | conj₃ => apply conj₃; simp_all [Intuitionistic];
+  | disj₃ => apply disj₃; simp_all [Intuitionistic];
+  | imply₁ => apply imply₁; simp_all [Intuitionistic];
+  | imply₂ => apply imply₂ <;> simp_all [Intuitionistic];
   | _ => simp_all;
 
 lemma sound! : (𝐄𝐅𝐐 ⊢! p) → (𝐈𝐧𝐭 W α) ⊧ p := λ ⟨d⟩ => sound d
