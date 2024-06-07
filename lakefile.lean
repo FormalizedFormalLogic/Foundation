ef4f783e4e5e2591d4102fa3e48ff715015ac3f1
SnO2WMaN <me@sno2wman.net>
2024-06-08 05:20:44 +0900

    revert

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ lean_lib «Logic» {
   -- add library configuration options here
 }
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "b5eba595428809e96f3ed113bc7ba776c5f801ac"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
 
 meta if get_config? env = some "dev" then
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "c7f4ac84b973b6efd8f24ba2b006cad1b32c9c53"
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
