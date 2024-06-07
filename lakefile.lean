8d18304affe4f45db00f9831ea93db1f19ea2a49
palalansouki <palalansouki@gmail.com>
2024-06-07 22:59:05 +0900

    fix?

--- lakefile.lean
+++ lakefile.lean
@@ -18,4 +18,4 @@ lean_exe «logic» {
 }
 
 meta if get_config? env = some "dev" then
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4"
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
