49e860224587b2125c27dcccd2e20f2799bf2ad7
SnO₂WMaN <me@sno2wman.net>
2024-06-08 06:47:51 +0900

    ci: Update CI files and environments (#58)
    * Update `ci.yml`
    
    * Update on PR
    
    * fx
    
    * fx?
    
    * fx
    
    * mmm...
    
    * revert
    
    * fx dep
    
    * Update

--- lakefile.lean
+++ lakefile.lean
@@ -5,17 +5,12 @@ package «logic» {
   -- add package configuration options here
 }
 
+@[default_target]
 lean_lib «Logic» {
   -- add library configuration options here
 }
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
-
-@[default_target]
-lean_exe «logic» {
-  root := `Main
-}
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
 
 meta if get_config? env = some "dev" then
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
