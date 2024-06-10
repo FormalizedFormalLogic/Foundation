6d9c1e11ad90b2a9beff036097864e4306ae398f
palalansouki <palalansouki@gmail.com>
2024-06-10 22:52:26 +0900

    fix

--- lakefile.lean
+++ lakefile.lean
@@ -5,17 +5,14 @@ package «arithmetization» {
   -- add any package configuration options here
 }
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "678f4912899df76cc45934a56bdf929ffe3cac50"
-
-meta if get_config? env = some "dev" then
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
-
-require logic from git "https://github.com/iehality/lean4-logic" @ "master"
-
 @[default_target]
 lean_lib «Arithmetization» {
   -- add any library configuration options here
 }
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "678f4912899df76cc45934a56bdf929ffe3cac50"
+
+require logic from git "https://github.com/iehality/lean4-logic" @ "master"
 
 require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
 
