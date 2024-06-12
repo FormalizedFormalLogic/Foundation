3fd1c8cb2852545e568542e0e72980ff54b8fdf7
palalansouki <palalansouki@gmail.com>
2024-06-12 20:57:41 +0900

    revert

--- lakefile.lean
+++ lakefile.lean
@@ -14,8 +14,6 @@ require mathlib from git "https://github.com/leanprover-community/mathlib4.git" 
 
 require logic from git "https://github.com/iehality/lean4-logic" @ "master"
 
-require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
-
 meta if get_config? env = some "dev" then
 require «doc-gen4» from git
   "https://github.com/leanprover/doc-gen4" @ "main"
