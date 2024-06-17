83ba7c654dab7b36eece4a50687b6d32a906fe03
palalansouki <palalansouki@gmail.com>
2024-06-18 00:30:50 +0900

    revert

--- lakefile.lean
+++ lakefile.lean
@@ -11,7 +11,7 @@ lean_lib «Logic» {
 }
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
-require importGraph from git "https://github.com/leanprover-community/import-graph" @ "main"
+require importGraph from git "https://github.com/leanprover-community/import-graph" @ "7983e959f8f4a79313215720de3ef1eca2d6d474"
 
 meta if get_config? env = some "ci" then
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
