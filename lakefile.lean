0873573d86a8e005a51b4256fe97d27d5a547ade
SnO2WMaN <me@sno2wman.net>
2024-07-01 19:54:38 +0900

    chore: Update to  v4.9.0

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ lean_lib «Logic» {
   -- add library configuration options here
 }
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "f0957a7575317490107578ebaee9efaf8e62a4ab"
 
 meta if get_config? env = some "ci" then
 require importGraph from git "https://github.com/leanprover-community/import-graph" @ "main"
