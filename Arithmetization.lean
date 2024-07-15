b19c3a8b335561de76746d09184ff18f7a2a2b2c
palalansouki <palalansouki@gmail.com>
2024-07-16 06:02:59 +0900

    add

--- Arithmetization.lean
+++ Arithmetization.lean
@@ -26,8 +26,15 @@ import Arithmetization.ISigmaOne.HFS.Fixpoint
 
 import Arithmetization.ISigmaOne.Metamath.Term.Basic
 import Arithmetization.ISigmaOne.Metamath.Term.Functions
+import Arithmetization.ISigmaOne.Metamath.Term.Typed
 
 import Arithmetization.ISigmaOne.Metamath.Formula.Basic
 import Arithmetization.ISigmaOne.Metamath.Formula.Functions
+import Arithmetization.ISigmaOne.Metamath.Formula.Iteration
+import Arithmetization.ISigmaOne.Metamath.Formula.Typed
+
+import Arithmetization.ISigmaOne.Metamath.Proof.Derivation
+import Arithmetization.ISigmaOne.Metamath.Proof.Theory
+import Arithmetization.ISigmaOne.Metamath.Proof.Typed
 
 import Arithmetization.ISigmaOne.Metamath.Coding
