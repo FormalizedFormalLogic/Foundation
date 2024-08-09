b883960cb1c0006d63e6a3deb279a23925644807
palalansouki <palalansouki@gmail.com>
2024-08-09 10:34:52 +0900

    fix

--- lakefile.lean
+++ lakefile.lean
@@ -15,7 +15,7 @@ require logic from git "https://github.com/FormalizedFormalLogic/Foundation" @ "
 require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4"@"v0.0.39"
 
 meta if get_config? env = some "ci" then
-require importGraph from git "https://github.com/leanprover-community/import-graph" @ "main"
+require importGraph from git "https://github.com/leanprover-community/import-graph" @ "68b518c9b352fbee16e6d632adcb7a6d0760e2b7"
 
 meta if get_config? env = some "ci" then
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "b941c425f6f0f1dc45fe13b850ffa7db1bb20d04"
