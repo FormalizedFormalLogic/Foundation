514ec01dd4fe6e2441ec6e1964c2b9ecfa4bdc3d
SnO2WMaN <me@sno2wman.net>
2024-09-12 21:55:37 +0900

    fx

--- Logic/Modal/LogicSymbol.lean
+++ Logic/Modal/LogicSymbol.lean
@@ -363,17 +363,17 @@ variable [Box F] [Dia F]
 @[simp] lemma dia_single : (◇'[p]) = [◇p] := by simp;
 
 
-lemma multibox_cons (hl : p ∉ l) : □'^[n](p :: l) ∼ □^[n]p :: □'^[n]l := by
+lemma multibox_cons (hl : p ∉ l) : □'^[n](p :: l) ~ □^[n]p :: □'^[n]l := by
   simp [List.multibox];
   apply Finset.toList_insert;
   simp_all;
-lemma box_cons (hl : p ∉ l) : □'(p :: l) ∼ □p :: □'l := by simpa using multibox_cons hl
+lemma box_cons (hl : p ∉ l) : □'(p :: l) ~ □p :: □'l := by simpa using multibox_cons hl
 
-lemma multidia_cons (hl : p ∉ l) : ◇'^[n](p :: l) ∼ ◇^[n]p :: ◇'^[n]l := by
+lemma multidia_cons (hl : p ∉ l) : ◇'^[n](p :: l) ~ ◇^[n]p :: ◇'^[n]l := by
   simp [List.multidia];
   apply Finset.toList_insert;
   simp_all;
-lemma dia_cons (hl : p ∉ l) : ◇'(p :: l) ∼ ◇p :: ◇'l := by simpa using multidia_cons hl
+lemma dia_cons (hl : p ∉ l) : ◇'(p :: l) ~ ◇p :: ◇'l := by simpa using multidia_cons hl
 
 
 lemma forall_multibox_of_subset_multibox (h : ∀ p ∈ l, p ∈ □''^[n]s) : ∀ p ∈ l, ∃ q ∈ s, p = □^[n]q := by
