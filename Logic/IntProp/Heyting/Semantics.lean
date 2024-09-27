cede3ecaa166254f8fcd5800d9471c408f608202
palalansouki <palalansouki@gmail.com>
2024-09-28 01:28:59 +0900

    soundness

--- Logic/IntProp/Heyting/Semantics.lean
+++ Logic/IntProp/Heyting/Semantics.lean
@@ -1,4 +1,5 @@
 import Logic.IntProp.Deduction
+import Logic.Vorspiel.Order
 
 namespace LO.IntProp
 
@@ -49,28 +50,76 @@ instance : HeytingAlgebra ℍ := ℍ.heyting
 
 instance : Nontrivial ℍ := ℍ.nontrivial
 
-def val (ℍ : HeytingSemantics α) (p : Formula α) : ℍ := p.hVal ℍ.valAtom
+def hVal (ℍ : HeytingSemantics α) (p : Formula α) : ℍ := p.hVal ℍ.valAtom
 
-scoped [LO.IntProp] infix:45 " ⊧ₕ " => LO.IntProp.HeytingSemantics.val
+scoped [LO.IntProp] infix:45 " ⊧ₕ " => LO.IntProp.HeytingSemantics.hVal
 
-@[simp] lemma val_verum : (ℍ ⊧ₕ ⊤) = ⊤ := rfl
+@[simp] lemma hVal_verum : (ℍ ⊧ₕ ⊤) = ⊤ := rfl
 
-@[simp] lemma val_falsum : (ℍ ⊧ₕ ⊥) = ⊥ := rfl
+@[simp] lemma hVal_falsum : (ℍ ⊧ₕ ⊥) = ⊥ := rfl
 
-@[simp] lemma val_and (p q : Formula α) : (ℍ ⊧ₕ p ⋏ q) = (ℍ ⊧ₕ p) ⊓ (ℍ ⊧ₕ q) := rfl
+@[simp] lemma hVal_and (p q : Formula α) : (ℍ ⊧ₕ p ⋏ q) = (ℍ ⊧ₕ p) ⊓ (ℍ ⊧ₕ q) := rfl
 
-@[simp] lemma val_or (p q : Formula α) : (ℍ ⊧ₕ p ⋎ q) = (ℍ ⊧ₕ p) ⊔ (ℍ ⊧ₕ q) := rfl
+@[simp] lemma hVal_or (p q : Formula α) : (ℍ ⊧ₕ p ⋎ q) = (ℍ ⊧ₕ p) ⊔ (ℍ ⊧ₕ q) := rfl
 
-@[simp] lemma val_imply (p q : Formula α) : (ℍ ⊧ₕ p ➝ q) = (ℍ ⊧ₕ p) ⇨ (ℍ ⊧ₕ q) := rfl
+@[simp] lemma hVal_imply (p q : Formula α) : (ℍ ⊧ₕ p ➝ q) = (ℍ ⊧ₕ p) ⇨ (ℍ ⊧ₕ q) := rfl
+
+@[simp] lemma hVal_iff (p q : Formula α) : (ℍ ⊧ₕ p ⭤ q) = bihimp (ℍ ⊧ₕ p) (ℍ ⊧ₕ q) := by simp [LogicalConnective.iff, bihimp, inf_comm]
+
+@[simp] lemma hVal_not (p : Formula α) : (ℍ ⊧ₕ ∼p) = (ℍ ⊧ₕ p)ᶜ := rfl
 
 instance : Semantics (Formula α) (HeytingSemantics α) := ⟨fun ℍ p ↦ (ℍ ⊧ₕ p) = ⊤⟩
 
-lemma val_def (ℍ : HeytingSemantics α) (p : Formula α) : ℍ ⊧ p ↔ p.hVal ℍ.valAtom = ⊤ := by rfl
+lemma val_def {ℍ : HeytingSemantics α} {p : Formula α} : ℍ ⊧ p ↔ p.hVal ℍ.valAtom = ⊤ := by rfl
+
+lemma val_def' {ℍ : HeytingSemantics α} {p : Formula α} : ℍ ⊧ p ↔ (ℍ ⊧ₕ p) = ⊤ := by rfl
 
 instance : Semantics.Top (HeytingSemantics α) := ⟨fun ℍ ↦ by simp [val_def]⟩
 
 instance : Semantics.Bot (HeytingSemantics α) := ⟨fun ℍ ↦ by simp [val_def]⟩
 
 instance : Semantics.And (HeytingSemantics α) := ⟨fun {ℍ p q} ↦ by simp [val_def]⟩
+
+@[simp] lemma val_imply {p q : Formula α} : ℍ ⊧ p ➝ q ↔ (ℍ ⊧ₕ p) ≤ (ℍ ⊧ₕ q) := by
+  simp [val_def]; rfl
+
+@[simp] lemma val_iff {p q : Formula α} : ℍ ⊧ p ⭤ q ↔ (ℍ ⊧ₕ p) = (ℍ ⊧ₕ q) := by
+  simp [LogicalConnective.iff, antisymm_iff]
+
+lemma val_not (p : Formula α) : ℍ ⊧ ∼p ↔ (ℍ ⊧ₕ p) = ⊥ := by simp [val_def]; rw [←HeytingAlgebra.himp_bot, himp_eq_top_iff, le_bot_iff]; rfl
+
+@[simp] lemma val_or (p q : Formula α) : ℍ ⊧ p ⋎ q ↔ (ℍ ⊧ₕ p) ⊔ (ℍ ⊧ₕ q) = ⊤ := by
+  simp [val_def]; rfl
+
+def mod (T : Hilbert α) : Set (HeytingSemantics α) := Semantics.models (HeytingSemantics α) T.axiomSet
+
+variable {T : Hilbert α}
+
+lemma mod_models_iff {p : Formula α} :
+    mod.{_,w} T ⊧ p ↔ ∀ ℍ : HeytingSemantics.{_,w} α, ℍ ⊧* T.axiomSet → ℍ ⊧ p := by
+  simp [mod, Semantics.models, Semantics.set_models_iff]
+
+lemma sound {p : Formula α} (d : T ⊢! p) : mod T ⊧ p := by
+  rcases d with ⟨d⟩
+  apply mod_models_iff.mpr fun ℍ hℍ ↦ ?_
+  induction d
+  case eaxm p hp =>
+    exact hℍ.RealizeSet hp
+  case mdp p q _ _ ihpq ihp =>
+    have : (ℍ ⊧ₕ p) ≤ (ℍ ⊧ₕ q) := by simpa using ihpq
+    simpa [val_def'.mp ihp] using this
+  case verum => simp
+  case imply₁ => simp
+  case imply₂ p q r => simp [himp_himp_inf_himp_inf_le]
+  case and₁ => simp
+  case and₂ => simp
+  case and₃ => simp
+  case or₁ => simp
+  case or₂ => simp
+  case or₃ => simp [himp_inf_himp_inf_sup_le]
+  case neg_equiv p =>
+    simp [Axioms.NegEquiv]
+
+instance : Sound T (mod T)  := ⟨sound⟩
 
 end HeytingSemantics
--- Logic/Vorspiel/Order.lean
+++ Logic/Vorspiel/Order.lean
@@ -1,5 +1,7 @@
 import Logic.Vorspiel.Vorspiel
 
+section
+
 variable {α : Sort u} (r : α → α → Prop)
 
 local infix:50 " ≺ " => r
@@ -25,3 +27,29 @@ lemma isInfiniteDescendingChain_of_non_acc (z : α) (hz : ¬Acc r z) :
       exact Classical.epsilon_spec this
   intro i; simpa using (this (i + 1)).1
 
+end
+
+section HeytingAlgebra
+
+variable {α : Type*} [HeytingAlgebra α]
+
+lemma himp_himp_inf_himp_inf_le (a b c : α) : (a ⇨ b ⇨ c) ⊓ (a ⇨ b) ⊓ a ≤ c := calc
+  (a ⇨ b ⇨ c) ⊓ (a ⇨ b) ⊓ a = (a ⇨ b ⇨ c) ⊓ b ⊓ a := by simp only [inf_assoc, himp_inf_self]
+  _                         = (a ⇨ b ⇨ c) ⊓ a ⊓ b := by simp only [inf_assoc, inf_comm a b]
+  _                         ≤ (b ⇨ c) ⊓ b         := by simp only [himp_inf_self a (b ⇨ c), le_inf_iff]
+                                                        constructor
+                                                        · simp only [inf_assoc, inf_le_left]
+                                                        · exact inf_le_right
+  _                         ≤ c                   := by simp
+
+lemma himp_inf_himp_inf_sup_le (a b c : α) : (a ⇨ c) ⊓ (b ⇨ c) ⊓ (a ⊔ b) ≤ c := by
+  have ha : a ≤ (a ⇨ c) ⊓ (b ⇨ c) ⇨ c := by
+    simp only [le_himp_iff, ← inf_assoc, inf_himp]
+    refine inf_le_of_left_le (by simp)
+  have hb : b ≤ (a ⇨ c) ⊓ (b ⇨ c) ⇨ c := by
+    simp only [le_himp_iff, inf_comm (a ⇨ c) (b ⇨ c), ← inf_assoc, inf_himp]
+    refine inf_le_of_left_le (by simp)
+  have : a ⊔ b ≤ (a ⇨ c) ⊓ (b ⇨ c) ⇨ c := sup_le_iff.mpr ⟨ha, hb⟩
+  simpa only [GeneralizedHeytingAlgebra.le_himp_iff, inf_comm (a ⊔ b)] using this
+
+end HeytingAlgebra
