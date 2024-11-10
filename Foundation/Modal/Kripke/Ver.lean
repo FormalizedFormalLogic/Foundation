54b1b162053d1225e1f2c6bdb794853c910f6dc3
SnO2WMaN <me@sno2wman.net>
2024-11-11 03:21:37 +0900

    Ver

--- Foundation/Modal/Kripke/Ver.lean
+++ Foundation/Modal/Kripke/Ver.lean
@@ -1,52 +1,40 @@
-import Foundation.Vorspiel.BinaryRelations
 import Foundation.Modal.Kripke.Completeness
-
-abbrev LO.Kripke.IsolatedFrameClass : FrameClass := λ F => Isolated F
 
 namespace LO.Modal
 
 namespace Kripke
 
-open LO.Kripke
-open System
-open Kripke
-open Formula
+abbrev IsolatedFrameClass : FrameClass := { F | Isolated F }
 
-variable {α : Type u}
-
-lemma axiomVer_defines : ∀ {F : Kripke.Frame}, (F#α ⊧* 𝗩𝗲𝗿 ↔ F ∈ IsolatedFrameClass) := by
-  simp [Kripke.ValidOnFrame];
+lemma IsolatedFrameClass.is_defined_by_Ver : IsolatedFrameClass.DefinedBy 𝗩𝗲𝗿 := by
   intro F;
+  simp [IsolatedFrameClass, Isolated, Semantics.RealizeSet];
   constructor;
+  . intro h φ V x y Rxy;
+    have := h Rxy;
+    contradiction;
   . intro h x y hxy;
     exact h ⊥ (λ _ _ => True) x _ hxy;
-  . intro hIrrefl _ _ x y hxy;
-    have := hIrrefl hxy;
-    contradiction;
-
-instance axiomVer_definability : 𝔽((𝗩𝗲𝗿 : Theory α)).DefinedBy (IsolatedFrameClass) where
-  define := axiomVer_defines
-  nonempty := by
-    use ⟨PUnit,  λ _ _ => False⟩
-    tauto;
-
-instance Ver_definability : 𝔽(Hilbert.Ver α).DefinedBy (IsolatedFrameClass) := inferInstance
-
-instance : Sound (Hilbert.Ver α) (IsolatedFrameClass#α) := inferInstance
-
-instance : System.Consistent (Hilbert.Ver α) := inferInstance
-
-variable [DecidableEq α]
-
-lemma isolated_CanonicalFrame {Ax : Theory α} (h : 𝗩𝗲𝗿 ⊆ Ax) [System.Consistent (Hilbert.ExtK Ax)] : Isolated (CanonicalFrame (Hilbert.ExtK Ax)) := by
-  intro x y rxy;
-  have : (CanonicalModel (Hilbert.ExtK Ax)) ⊧ □⊥ := iff_valid_on_canonicalModel_deducible.mpr $ (Hilbert.ExtK.maxm!) (by apply h; simp);
-  exact this x _ rxy;
-
-instance : Complete (Hilbert.Ver α) (IsolatedFrameClass.{u}#α) := instComplete_of_mem_canonicalFrame IsolatedFrameClass $ by
-  apply isolated_CanonicalFrame;
-  tauto;
 
 end Kripke
+
+namespace Hilbert
+
+instance Ver.Kripke.sound : Sound (Hilbert.Ver ℕ) (Kripke.IsolatedFrameClass) := Kripke.instSound Kripke.IsolatedFrameClass.is_defined_by_Ver rfl
+
+instance Ver.consistent : System.Consistent (Hilbert.Ver ℕ) := Kripke.instConsistent (C := Kripke.IsolatedFrameClass) $ by
+  use Kripke.irreflexivePointFrame;
+  tauto;
+
+lemma Kripke.isolated_CanonicalFrame (h : 𝗩𝗲𝗿 ⊆ Ax) [System.Consistent (Hilbert.ExtK Ax)] : Isolated (canonicalFrame (Hilbert.ExtK Ax)) := by
+  intro x y Rxy;
+  have : (canonicalModel (Hilbert.ExtK Ax)) ⊧ □⊥ := iff_valid_on_canonicalModel_deducible.mpr $ (Hilbert.ExtK.maxm!) (by apply h; simp);
+  exact this x _ Rxy;
+
+instance Ver.Kripke.complete : Complete (Hilbert.Ver ℕ) (Kripke.IsolatedFrameClass) := Kripke.instCompleteOfCanonical (C := Kripke.IsolatedFrameClass) $ by
+  apply Kripke.isolated_CanonicalFrame;
+  tauto;
+
+end Hilbert
 
 end LO.Modal
