2aaf4008519bbb84bc3fc6570bba2a8203f1d312
SnO2WMaN <me@sno2wman.net>
2025-02-13 03:03:45 +0900

    proof sketch

--- Foundation/Modal/Kripke/KH_Incompleteness.lean
+++ Foundation/Modal/Kripke/KH_Incompleteness.lean
@@ -1,4 +1,6 @@
 import Foundation.Modal.Kripke.AxiomL
+import Foundation.Modal.Hilbert.WellKnown
+
 
 namespace Set
 
@@ -77,6 +79,76 @@ lemma valid_atomic_Four_of_valid_atomic_H : F ⊧ Axioms.H (atom 0) → F ⊧ Ax
   trans;
   . exact valid_atomic_L_iff_valid_atomic_H.mpr;
   . exact valid_atomic_4_of_valid_atomic_L;
+
+abbrev cresswellFrame : Kripke.Frame where
+  World := ℕ × Fin 2
+  Rel n m :=
+    match n, m with
+    | (n, 0), (m, 0) => n < m
+    | (n, 1), (m, 1) => n ≤ m + 1
+    | (_, 0), (_, 1) => True
+    | _, _ => False
+
+namespace cresswellFrame
+
+abbrev Sharp := { w : cresswellFrame.World // w.2 = 0 }
+@[match_pattern] abbrev sharp (n : ℕ) : Sharp := ⟨(n, 0), rfl⟩
+postfix:max "♯" => sharp
+
+abbrev Flat := { w : cresswellFrame.World // w.2 = 1 }
+@[match_pattern] abbrev flat (n : ℕ) : Flat := ⟨(n, 1), rfl⟩
+postfix:max "♭" => flat
+
+end cresswellFrame
+
+
+abbrev cresswellModel : Kripke.Model := ⟨cresswellFrame, λ w _ => w ≠ 0♭⟩
+
+lemma not_valid_axiomFour_in_cresswellModel : ¬(Satisfies cresswellModel 2♭ (Axioms.Four (atom 0))) := by
+  apply Satisfies.imp_def.not.mpr;
+  push_neg;
+  constructor;
+  . intro x;
+    match x with
+    | (_, 0) => simp;
+    | (_, 1) => simp [Satisfies]; omega;
+  . apply Satisfies.box_def.not.mpr
+    push_neg;
+    use 1♭;
+    constructor;
+    . omega;
+    . apply Satisfies.box_def.not.mpr;
+      push_neg;
+      use 0♭;
+      constructor;
+      . omega;
+      . tauto;
+
+lemma valid_axiomH_in_cresswellModel : cresswellModel ⊧ □(□φ ⭤ φ) ➝ □φ := by
+  sorry;
+
+lemma provable_KH_of_valid_cresswellModel : Hilbert.KH ⊢! φ → cresswellModel ⊧ φ := by
+  intro h;
+  induction h using Hilbert.Deduction.rec! with
+  | maxm h =>
+    rcases (by simpa using h) with (⟨_, rfl⟩ | ⟨_, rfl⟩);
+    . exact Kripke.ValidOnModel.axiomK;
+    . exact valid_axiomH_in_cresswellModel;
+  | mdp ihφψ ihφ => exact Kripke.ValidOnModel.mdp ihφψ ihφ;
+  | nec ihφ => exact Kripke.ValidOnModel.nec ihφ;
+  | imply₁ => exact Kripke.ValidOnModel.imply₁;
+  | imply₂ => exact Kripke.ValidOnModel.imply₂;
+  | ec => exact Kripke.ValidOnModel.elimContra;
+
+lemma KH_unprov_axiomFour : Hilbert.KH ⊬ Axioms.Four (atom 0) := fun hC =>
+  not_valid_axiomFour_in_cresswellModel (provable_KH_of_valid_cresswellModel hC ↑2♭)
+
+theorem KH_KripkeIncomplete : ¬∃ C : Kripke.FrameClass, ∀ φ, (Hilbert.KH ⊢! φ ↔ C ⊧ φ) := by
+  rintro ⟨C, h⟩;
+  have : C ⊧ Axioms.H (atom 0) := @h (Axioms.H (atom 0)) |>.mp $ by simp;
+  have : C ⊧ Axioms.Four (atom 0) := fun {F} hF => valid_atomic_Four_of_valid_atomic_H (this hF);
+  have : Hilbert.KH ⊢! Axioms.Four (atom 0) := @h (Axioms.Four (atom 0)) |>.mpr this;
+  exact @KH_unprov_axiomFour this;
 
 end Kripke
 
