94d18217bf9b11d3a0b1944424b1e028e50710a3
SnO₂WMaN <me@sno2wman.net>
2025-02-17 04:52:14 +0900

    Add KB4 ⊂ KTc (#210)

--- Foundation/Modal/Logic/Sublogic.lean
+++ Foundation/Modal/Logic/Sublogic.lean
@@ -788,7 +788,7 @@ instance : ProperSublogic Logic.KTc Logic.Triv := ⟨by
 instance : ProperSublogic Logic.KTc Logic.Ver := ⟨by
   constructor;
   . rw [KTc.eq_CoreflexiveKripkeFrameClass_Logic, Ver.eq_IsolatedFrameClass_Logic];
-    rintro φ hφ F F_corefl;
+    rintro φ hφ F F_iso;
     apply hφ;
     simp_all [Coreflexive, Isolated];
   . suffices ∃ φ, Hilbert.Ver ⊢! φ ∧ ¬CoreflexiveFrameClass ⊧ φ by
@@ -803,6 +803,35 @@ instance : ProperSublogic Logic.KTc Logic.Ver := ⟨by
       . simp [M, Coreflexive]; aesop;
       . suffices ∃ x, (0 : M.World) ≺ x by simpa [Satisfies, Semantics.Realize];
         use 0;
+⟩
+
+instance : ProperSublogic Logic.KB4 Logic.KTc := ⟨by
+  constructor;
+  . rw [KB4.eq_ReflexiveTransitiveKripkeFrameClass_Logic, KTc.eq_CoreflexiveKripkeFrameClass_Logic];
+    rintro φ hφ F F_corefl;
+    apply hφ;
+    refine ⟨?_, ?_⟩;
+    . intro x y Rxy;
+      rw [F_corefl Rxy] at *;
+      assumption;
+    . intro x y z Rxy Ryz;
+      rw [F_corefl Rxy, F_corefl Ryz] at *;
+      assumption;
+  . suffices ∃ φ, Hilbert.KTc ⊢! φ ∧ ¬SymmetricTransitiveFrameClass ⊧ φ by
+      simpa [KB4.eq_ReflexiveTransitiveKripkeFrameClass_Logic];
+    use (Axioms.Tc (.atom 0));
+    constructor;
+    . exact axiomTc!;
+    . apply Formula.Kripke.ValidOnFrameClass.not_of_exists_model_world;
+      let M : Model := ⟨⟨Fin 2, λ x y => True⟩, λ w _ => w = 0⟩;
+      use M, 0;
+      constructor;
+      . simp [Symmetric, Transitive, M];
+      . suffices ∃ x, (x : M.World) ≠ 0 by
+          simp [M, Semantics.Realize, Satisfies];
+          tauto;
+        use 1;
+        aesop;
 ⟩
 
 end LO.Modal.Logic
