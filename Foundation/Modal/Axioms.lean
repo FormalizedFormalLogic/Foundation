c63a2879a0f6ee219d698a6c5c1bc9b1af25791b
SnO2WMaN <me@sno2wman.net>
2025-03-05 18:54:04 +0900

    Remove old axiom setups

--- Foundation/Modal/Axioms.lean
+++ Foundation/Modal/Axioms.lean
@@ -6,113 +6,83 @@ namespace LO.Axioms
 variable {F : Type*} [BasicModalLogicalConnective F]
 variable (φ ψ χ : F)
 
-
-section Basic
-
 /-- `◇` is duality of `□`. -/
-protected abbrev DiaDuality [Dia F] := ◇φ ⭤ ∼(□(∼φ))
-abbrev DiaDuality.set [Dia F] : Set F := { Axioms.DiaDuality φ | (φ) }
+protected abbrev DiaDuality := ◇φ ⭤ ∼(□(∼φ))
 
 protected abbrev K := □(φ ➝ ψ) ➝ □φ ➝ □ψ
-abbrev K.set : Set F := { Axioms.K φ ψ | (φ) (ψ) }
-notation:max "𝗞" => K.set
 
 /-- Axiom for reflexive -/
 protected abbrev T := □φ ➝ φ
-abbrev T.set : Set F := { Axioms.T φ | (φ) }
-notation:max "𝗧" => T.set
 
+/-- Alternative axiom `T` -/
 protected abbrev DiaTc := φ ➝ ◇φ
 
 /-- Axiom for symmetric -/
-protected abbrev B [Dia F] := φ ➝ □◇φ
-abbrev B.set [Dia F] : Set F := { Axioms.B φ | (φ) }
-notation:max "𝗕" => B.set
-
-/-- `□`-only version of axiom `𝗕`. -/
-protected abbrev B₂ := □φ ➝ □(∼□(∼φ))
-abbrev B₂.set : Set F := { Axioms.B₂ φ | (φ) }
-notation:max "𝗕(□)" => B₂.set
+protected abbrev B := φ ➝ □◇φ
 
 /-- Axiom for serial -/
-protected abbrev D [Dia F] := □φ ➝ ◇φ
-abbrev D.set [Dia F] : Set F := { Axioms.D φ | (φ) }
-notation:max "𝗗" => D.set
+protected abbrev D := □φ ➝ ◇φ
 
-
+/-- Alternative axiom `D` -/
 protected abbrev P : F := ∼(□⊥)
-abbrev P.set : Set F := { Axioms.P | }
-notation:max "𝗣" => P.set
-@[simp] lemma P.set.def : 𝗣 = {(∼(□⊥) : F)} := by ext; simp;
 
 /-- Axiom for transivity -/
 protected abbrev Four := □φ ➝ □□φ
-abbrev Four.set : Set F := { Axioms.Four φ | (φ) }
-notation:max "𝟰" => Four.set
 
 /-- Axiom for euclidean -/
-protected abbrev Five [Dia F] := ◇φ ➝ □◇φ
-abbrev Five.set [Dia F] : Set F := { Axioms.Five φ | (φ) }
-notation:max "𝟱" => Five.set
-
-/-- `□`-only version of axiom `𝟱`. -/
-protected abbrev Five₂ := ∼□φ ➝ □(∼□(∼φ))
-abbrev Five₂.set : Set F := { Axioms.Five₂ φ | (φ) }
-notation:max "𝟱(□)" => Five₂.set
+protected abbrev Five := ◇φ ➝ □◇φ
 
 /-- Axiom for confluency -/
-protected abbrev Point2 [Dia F] := ◇□φ ➝ □◇φ
-abbrev Point2.set [Dia F] : Set F := { Axioms.Point2 φ | (φ) }
-notation:max ".𝟮" => Point2.set
+protected abbrev Point2 := ◇□φ ➝ □◇φ
 
+/-- Axiom for weak confluency -/
 protected abbrev WeakPoint2 := ◇(□φ ⋏ ψ) ➝ □(◇φ ⋎ ψ)
 
 /-- Axiom for density -/
 protected abbrev C4 := □□φ ➝ □φ
-abbrev C4.set : Set F := { Axioms.C4 φ | (φ) }
-notation:max "𝗖𝟰" => C4.set
 
 /-- Axiom for functionality -/
-protected abbrev CD [Dia F] := ◇φ ➝ □φ
-abbrev CD.set [Dia F] : Set F := { Axioms.CD φ | (φ) }
-notation:max "𝗖𝗗" => CD.set
+protected abbrev CD := ◇φ ➝ □φ
 
 /-- Axiom for coreflexivity -/
 protected abbrev Tc := φ ➝ □φ
-abbrev Tc.set : Set F := { Axioms.Tc φ | (φ) }
-notation:max "𝗧𝗰" => Tc.set
 
+/-- Alternative axiom `Tc` -/
 protected abbrev DiaT := ◇φ ➝ φ
 
+/-- Axiom for isolated -/
 protected abbrev Ver := □φ
-abbrev Ver.set : Set F := { Axioms.Ver φ | (φ) }
-notation:max "𝗩𝗲𝗿" => Ver.set
 
+/-- Axiom for connectivity -/
 protected abbrev Point3 := □(□φ ➝ ψ) ⋎ □(□ψ ➝ φ)
-abbrev Point3.set : Set F := { Axioms.Point3 φ ψ | (φ) (ψ) }
 
+/-- Axiom for weak connectivity -/
 protected abbrev WeakPoint3 := □(⊡φ ➝ ψ) ⋎ □(⊡ψ ➝ φ)
 
+/--
+  Axiom for
+  - weakly converse wellfounded partial order (for non-resritcted Kripke frame)
+  - partial order (for finite Kripke frame)
+-/
 protected abbrev Grz := □(□(φ ➝ □φ) ➝ φ) ➝ φ
-abbrev Grz.set : Set F := { Axioms.Grz φ | (φ) }
-notation:max "𝗚𝗿𝘇" => Grz.set
 
-protected abbrev M [Dia F] := (□◇φ ➝ ◇□φ)
-abbrev M.set [Dia F] : Set F := { Axioms.M φ | (φ) }
-notation:max "𝗠" => M.set
+/--
+  Axiom for McKinsey condition
+-/
+protected abbrev M := (□◇φ ➝ ◇□φ)
 
+/--
+  Axiom for
+  - transitive converse wellfounded order (for non-resritcted Kripke frame)
+  - strict partial order (for finite Kripke frame)
+-/
 protected abbrev L := □(□φ ➝ φ) ➝ □φ
-abbrev L.set : Set F := { Axioms.L φ | (φ) }
-notation:max "𝗟" => L.set
 
 protected abbrev H := □(□φ ⭤ φ) ➝ □φ
-abbrev H.set : Set F := { Axioms.H φ | (φ) }
-notation:max "𝗛" => H.set
 
-end Basic
-
+/--
+  Axiom for Geach confluency.
+-/
 protected abbrev Geach (t : Geachean.Taple) (φ : F) := ◇^[t.i](□^[t.m]φ) ➝ □^[t.j](◇^[t.n]φ)
-abbrev Geach.set (t : Geachean.Taple) : Set F := { Axioms.Geach t φ | (φ) }
-notation:max "𝗴𝗲(" t ")" => Geach.set t
 
 end LO.Axioms
