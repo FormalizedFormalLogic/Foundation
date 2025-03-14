4a322c7aba31aac8e06822e6cd3ce9e2780b883f
SnO2WMaN <me@sno2wman.net>
2025-03-14 14:31:02 +0900

    Grz.2 < Grz.3

--- Foundation/Modal/Logic/Sublogic/Grz.lean
+++ Foundation/Modal/Logic/Sublogic/Grz.lean
@@ -47,4 +47,67 @@ theorem Grz_ssubset_Triv : Logic.Grz ⊂ Logic.Triv := by
   exact S5Grz_eq_Triv.symm;
 instance : ProperSublogic Logic.Grz Logic.Triv := ⟨Grz_ssubset_Triv⟩
 
+
+instance : ProperSublogic Logic.Grz Logic.GrzPoint2 := ⟨by
+  constructor;
+  . exact Hilbert.weakerThan_of_dominate_axioms (by simp) |>.subset;
+  . sorry;
+⟩
+
+instance : ProperSublogic Logic.GrzPoint2 Logic.GrzPoint3 := ⟨by
+  constructor;
+  . rw [GrzPoint2.eq_ReflexiveTransitiveAntiSymmetricConfluentFiniteKripkeFrameClass_Logic, GrzPoint3.eq_ReflexiveTransitiveAntiSymmetricConnectedFiniteKripkeFrameClass_Logic];
+    rintro φ hφ F ⟨_, F_refl, F_trans, F_antisymm, F_conn⟩;
+    apply hφ;
+    refine ⟨inferInstance, F_refl, F_trans, F_antisymm, ?_⟩;
+    . rintro x y z ⟨Rxy, Ryz⟩;
+      rcases F_conn ⟨Rxy, Ryz⟩ with (Ryz | Rzy);
+      . use z; tauto;
+      . use y; tauto
+  . suffices ∃ φ, Hilbert.GrzPoint3 ⊢! φ ∧ ¬Kripke.FrameClass.finite_confluent_partial_order ⊧ φ by
+      simpa [GrzPoint2.eq_ReflexiveTransitiveAntiSymmetricConfluentFiniteKripkeFrameClass_Logic];
+    use Axioms.Point3 (.atom 0) (.atom 1);
+    constructor;
+    . simp;
+    . apply Kripke.not_validOnFrameClass_of_exists_model_world;
+      let F : Frame := ⟨Fin 4, λ x y => x = 0 ∨ x = y ∨ y = 3⟩;
+      let M : Model := ⟨
+        F,
+        λ x a => match a with | 0 => (1 : F.World) ≺ x | 1 => (2 : F.World) ≺ x | _ => False
+      ⟩;
+      use M, 0;
+      constructor;
+      . simp only [Fin.isValue, Set.mem_setOf_eq, M, F];
+        refine ⟨?_, ?_, ?_, ?_, ?_⟩;
+        . tauto;
+        . simp [Reflexive];
+        . simp [Transitive]; omega;
+        . simp [AntiSymmetric]; omega;;
+        . rintro x y z ⟨(_ | _ | Rxy), (_ | _ | Rxy)⟩;
+          repeat { use 3; tauto; }
+      . apply Satisfies.or_def.not.mpr
+        push_neg;
+        constructor;
+        . apply Satisfies.box_def.not.mpr;
+          push_neg;
+          use 1;
+          constructor;
+          . tauto;
+          . apply Satisfies.imp_def₂.not.mpr;
+            push_neg;
+            constructor;
+            . tauto;
+            . simp [M, Semantics.Realize, Satisfies, Frame.Rel', F];
+        . apply Satisfies.box_def.not.mpr;
+          push_neg;
+          use 2;
+          constructor;
+          . tauto;
+          . apply Satisfies.imp_def₂.not.mpr;
+            push_neg;
+            constructor;
+            . tauto;
+            . simp [M, Semantics.Realize, Satisfies, Frame.Rel', F];
+⟩
+
 end LO.Modal.Logic
--- Foundation/Modal/Logic/Sublogic/ModalCube.lean
+++ Foundation/Modal/Logic/Sublogic/ModalCube.lean
@@ -569,10 +569,4 @@ lemma KD_ssubset_S4 : Logic.KD ⊂ Logic.S4 := by
   . exact KD_ssubset_KT;
   . exact KT_ssubset_S4;
 
-instance : ProperSublogic Logic.Grz Logic.GrzPoint2 := ⟨by
-  constructor;
-  . exact Hilbert.weakerThan_of_dominate_axioms (by simp) |>.subset;
-  . sorry;
-⟩
-
 end LO.Modal.Logic
