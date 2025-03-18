3bc97972be9f35b9221af5a1b7446ecbaeaf9bdc
SnO2WMaN <me@sno2wman.net>
2025-03-18 18:59:32 +0900

    done.

--- Foundation/Modal/ModalCompanion/Cl.lean
+++ Foundation/Modal/ModalCompanion/Cl.lean
@@ -16,25 +16,20 @@ section S5
 
 namespace Logic
 
-noncomputable instance : Entailment.S4 Hilbert.S5 where
-  Four φ := Logic.S4_ssubset_S5.1 (by simp) |>.some;
-
 open Formula.Kripke in
-lemma mem_gAxiomFive_smallestMC_of_Cl : (Axioms.Five (.atom 0)) ∈ Logic.Cl.smallestMC := by
+lemma S5.is_smallestMC_of_Cl.lemma₁ : (◇□(.atom 0) ➝ □(.atom 0)) ∈ Logic.Cl.smallestMC := by
   have : □(.atom 0) ⋎ □(∼□(.atom 0)) ∈ Logic.Cl.smallestMC := by
     apply Logic.sumNormal.mem₂;
     use Axioms.LEM (.atom 0);
     constructor;
     . simp;
     . tauto;
-  apply Propositional.Logic.smallestMC.mdp_S4 ?_
-    $ @Logic.subst Logic.Cl.smallestMC (by sorry) _ (λ _ => ∼(.atom 0)) this;
+  apply Propositional.Logic.smallestMC.mdp_S4 ?_ this;
   apply imp_trans''! ?_ imply_of_not_or!;
+  apply imp_trans''! ?_ or_comm!;
   apply or_replace!;
+  . simp;
   . apply contra₁'!;
-    exact diaDuality_mp!;
-  . apply axiomK'!;
-    apply nec!;
     exact diaDuality_mp!;
 
 lemma S5.is_smallestMC_of_Cl : Logic.S5 = Logic.Cl.smallestMC := by
@@ -46,7 +41,17 @@ lemma S5.is_smallestMC_of_Cl : Logic.S5 = Logic.Cl.smallestMC := by
       rcases (by simpa using h) with (⟨s, rfl⟩ | ⟨s, rfl⟩ | ⟨s, rfl⟩);
       . apply Logic.sumNormal.mem₁; simp;
       . apply Logic.sumNormal.mem₁; simp;
-      . exact Logic.sumNormal.subst $ mem_gAxiomFive_smallestMC_of_Cl;
+      . have := Logic.sumNormal.subst (s := λ _ => ∼(s 0)) $ S5.is_smallestMC_of_Cl.lemma₁;
+        apply Propositional.Logic.smallestMC.mdp_S4 ?_ this;
+        simp;
+        apply imp_trans''! ?_ imply_of_not_or!;
+        apply imp_trans''! not_or_of_imply! ?_;
+        apply imp_trans''! ?_ or_comm!;
+        apply or_replace!;
+        . apply contra₂'!;
+          exact diaDuality_mp!;
+        . apply contra₁'!;
+          exact diaDuality_mp!;
     | mdp => apply Logic.sumNormal.mdp <;> assumption;
     | nec => apply Logic.sumNormal.nec; assumption;
     | _ => apply Logic.sumNormal.mem₁; simp;
@@ -61,6 +66,21 @@ lemma S5.is_smallestMC_of_Cl : Logic.S5 = Logic.Cl.smallestMC := by
       haveI : Hilbert.S4 ⊢! ◇φᵍ := iff_provable_Cl_provable_dia_gS4.mp hφ;
       haveI : Hilbert.S4 ⊢! ◇□φᵍ := (diaK'! $ Hilbert.goedelTranslated_axiomTc) ⨀ this;
       exact rm_diabox'! $ Logic.S4_ssubset_S5.1 this;
+
+instance modalCompanion_Cl_S5 : ModalCompanion Logic.Cl Logic.S5 := by
+  rw [Logic.S5.is_smallestMC_of_Cl];
+  exact Modal.instModalCompanion_of_smallestMC_via_KripkeSemantics
+    (IC := Propositional.Kripke.FrameClass.euclidean)
+    (MC := Modal.Kripke.FrameClass.refl_eucl)
+    (by rw [Propositional.Logic.Cl.Kripke.eq_euclidean])
+    (by rw [←Logic.S5.is_smallestMC_of_Cl, ←Logic.S5.eq_ReflexiveEuclideanKripkeFrameClass_Logic])
+    (by
+      simp;
+      intro F hF;
+      constructor;
+      . infer_instance;
+      . infer_instance;
+    );
 
 end Logic
 
