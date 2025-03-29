1a4501d0f760663c926314421ff673a77a564620
SnO2WMaN <me@sno2wman.net>
2025-03-30 07:16:32 +0900

    feat(Modal/Kripke): FFP of Ver
    Fixes #296

--- Foundation/Modal/Kripke/Hilbert/Ver.lean
+++ Foundation/Modal/Kripke/Hilbert/Ver.lean
@@ -1,6 +1,7 @@
 import Foundation.Modal.Kripke.AxiomVer
 import Foundation.Modal.Kripke.Hilbert.Soundness
 import Foundation.Modal.Hilbert.WellKnown
+import Foundation.Modal.Kripke.Rooted
 
 namespace LO.Modal
 
@@ -8,16 +9,23 @@ open Kripke
 open Hilbert.Kripke
 
 protected abbrev Kripke.FrameClass.isolated : FrameClass := { F | IsIsolated _ F }
+protected abbrev Kripke.FrameClass.finite_isolated : FrameClass := { F | Finite F.World ∧ IsIsolated _ F }
 
-namespace Hilbert.Ver
+namespace Hilbert.Ver.Kripke
 
-instance Kripke.sound : Sound (Hilbert.Ver) FrameClass.isolated := instSound_of_validates_axioms $ by
+instance sound : Sound (Hilbert.Ver) FrameClass.isolated := instSound_of_validates_axioms $ by
   apply FrameClass.Validates.withAxiomK;
   rintro F hF _ (rfl | rfl);
   have := Set.mem_setOf_eq.mp hF;
   exact Kripke.validate_AxiomVer_of_isolated (F := F);
 
-instance Kripke.consistent : Entailment.Consistent (Hilbert.Ver) := consistent_of_sound_frameclass FrameClass.isolated $ by
+instance sound_finite_isolated : Sound (Hilbert.Ver) Kripke.FrameClass.finite_isolated :=
+  instSound_of_validates_axioms $ by
+    apply FrameClass.Validates.withAxiomK;
+    rintro F ⟨_, _⟩ _ (rfl | rfl);
+    exact validate_AxiomVer_of_isolated (F := F);
+
+instance consistent : Entailment.Consistent (Hilbert.Ver) := consistent_of_sound_frameclass FrameClass.isolated $ by
   use blackpoint;
   apply Set.mem_setOf_eq.mpr;
   infer_instance;
@@ -27,8 +35,32 @@ instance : Kripke.Canonical (Hilbert.Ver) FrameClass.isolated := ⟨by
   infer_instance;
 ⟩
 
-instance Kripke.complete : Complete (Hilbert.Ver) FrameClass.isolated := inferInstance
+instance complete : Complete (Hilbert.Ver) FrameClass.isolated := inferInstance
 
-end Hilbert.Ver
+instance complete_finite_isolated : Complete (Hilbert.Ver) Kripke.FrameClass.finite_isolated := ⟨by
+  intro φ hφ;
+  apply Kripke.complete.complete;
+  intro F ⟨F_iso⟩ V r;
+  apply Model.pointGenerate.modal_equivalent_at_root (r := r) |>.mp;
+  apply hφ;
+  refine ⟨?_, ?_⟩;
+  . apply finite_iff_exists_equiv_fin.mpr;
+    use 1;
+    constructor;
+    trans Unit;
+    . refine ⟨λ _ => (), λ _ => ⟨r, by tauto⟩, ?_, ?_⟩
+      . simp [Function.LeftInverse];
+        intro x Rrx;
+        exfalso;
+        induction Rrx with
+        | single h => exact F_iso h;
+        | tail _ h => exact F_iso h;
+      . simp [Function.RightInverse, Function.LeftInverse];
+    . exact finOneEquiv.symm;
+  . apply isIsolated_iff _ _ |>.mpr;
+    rintro ⟨x, (rfl | hx)⟩ ⟨y, (rfl | hy)⟩ <;> apply F_iso;
+⟩
+
+end Hilbert.Ver.Kripke
 
 end LO.Modal
