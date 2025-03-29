a08845d9b3ccd49ee81a217f654fda940d545a7a
SnO2WMaN <me@sno2wman.net>
2025-03-29 19:04:22 +0900

    GL_ssubset_GLPoint3

--- Foundation/Modal/Logic/Sublogic/GL.lean
+++ Foundation/Modal/Logic/Sublogic/GL.lean
@@ -1,4 +1,4 @@
-import Foundation.Modal.Logic.WellKnown
+import Foundation.Modal.Logic.Sublogic.K4
 
 namespace LO.Modal.Logic
 
@@ -28,5 +28,24 @@ theorem GL_ssubset_Ver : Logic.GL ⊂ Logic.Ver := by
       have := Hilbert.GL.Kripke.consistent.not_bot;
       contradiction
 instance : ProperSublogic Logic.GL Logic.Ver := ⟨GL_ssubset_Ver⟩
+
+theorem GL_ssubset_GLPoint3 : Logic.GL ⊂ Logic.GLPoint3 := by
+  constructor;
+  . exact Hilbert.weakerThan_of_dominate_axioms (by simp) |>.subset;
+  . suffices ∃ φ, Hilbert.GLPoint3 ⊢! φ ∧ ¬Kripke.FrameClass.finite_trans_irrefl ⊧ φ by
+      simpa [GL.eq_TransitiveIrreflexiveFiniteKripkeFrameClass_Logic];
+    use (Axioms.WeakPoint3 (.atom 0) (.atom 1));
+    constructor;
+    . simp;
+    . let M : Model := ⟨⟨Fin 3, λ x y => (x = 0 ∧ y = 1) ∨ (x = 0 ∧ y = 2)⟩, (λ w a => match a with | 0 => w = 1 | 1 => w = 2 | _ => False)⟩;
+      apply Kripke.not_validOnFrameClass_of_exists_model_world;
+      use M, 0;
+      constructor;
+      . refine ⟨inferInstance, ⟨by omega⟩, ⟨by omega⟩⟩
+      . suffices (0 : M.World) ≺ 1 ∧ (∀ x, (1 : M.World) ≺ x → x = 1) ∧ (0 : M.World) ≺ 2 ∧ ∀ x, (2 : M.World) ≺ x → x = 2 by
+          simpa [Semantics.Realize, Satisfies, ValidOnFrame, M];
+        refine ⟨?_, ?_, ?_, ?_⟩;
+        all_goals omega;
+instance : ProperSublogic Logic.GL Logic.GLPoint3 := ⟨GL_ssubset_GLPoint3⟩
 
 end LO.Modal.Logic
