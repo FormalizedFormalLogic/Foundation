dcf5e569be5df2bc9df022505e1c453417a5a248
SnO2WMaN <me@sno2wman.net>
2025-04-18 03:18:32 +0900

    refactor(Modal/Kripke): Integrate `Kripke/ExtendRoot` and `Kripke/ExtendRoot₂`
    Fixes #330

---
+++ Foundation/Vorspiel/Finset/Card.lean
@@ -0,0 +1,31 @@
+import Foundation.Modal.Boxdot.Basic
+import Foundation.Modal.Kripke.Tree
+import Foundation.Vorspiel.List.Chain
+import Mathlib.Algebra.Order.BigOperators.Group.Finset
+import Mathlib.Data.Finite.Sum
+
+namespace Finset
+
+variable {α} {s t : Finset α}
+
+lemma ssubset_of_subset_lt_card
+  (h_subset : s ⊆ t)
+  (h_card_le : s.card < t.card) : s ⊂ t := by
+  constructor;
+  . assumption;
+  . by_contra hC;
+    have : t = s := Finset.eq_iff_card_le_of_subset hC |>.mp (by omega);
+    subst this;
+    simp at h_card_le;
+
+lemma eq_card_of_eq (h : s = t) : s.card = t.card := by tauto;
+
+lemma sum_le_card
+  {n} {f : α → ℕ}
+  (hf : ∀ a ∈ s, f a ≤ n)
+  : (∑ a ∈ s, f a) ≤ s.card * n :=
+  calc
+    _ ≤ (∑ x ∈ s, n) := by apply Finset.sum_le_sum hf
+    _ = s.card * n   := by simp_all;
+
+end Finset
