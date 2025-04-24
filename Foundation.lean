493faaa1c35ff2cf4104d18aebaeccb1805133db
SnO2WMaN <me@sno2wman.net>
2025-04-25 07:56:17 +0900

    `NatLTFrame_validates_AxiomZ`

--- Foundation.lean
+++ Foundation.lean
@@ -70,6 +70,7 @@ import Foundation.Modal.Kripke.NNFormula
 import Foundation.Modal.Kripke.ComplexityLimited
 import Foundation.Modal.Kripke.Undefinability
 import Foundation.Modal.Kripke.AxiomZ
+import Foundation.Modal.Kripke.LinearFrame
 
 import Foundation.Modal.PLoN.Hilbert.N
 
--- Foundation/Modal/Kripke/LinearFrame.lean
+++ Foundation/Modal/Kripke/LinearFrame.lean
@@ -1,5 +1,8 @@
 import Foundation.Modal.Kripke.Preservation
 import Foundation.Modal.Kripke.Rooted
+import Foundation.Modal.Kripke.AxiomZ
+import Mathlib.Order.Interval.Finset.Defs
+import Mathlib.Order.Interval.Finset.Nat
 
 namespace LO.Modal.Kripke
 
@@ -91,5 +94,50 @@ instance : IsAntisymm _ (FinLEFrame n) := by
 
 end FinLEFrame
 
+
+open
+  Formula
+  Formula.Kripke
+in
+/-- Goldblatt, Exercise 8.1 (1) -/
+lemma NatLTFrame_validates_AxiomZ : NatLTFrame ⊧ (Axiom.Z (.atom 0)) := by
+  intro V x hx₁ hx₂ y lt_xy;
+  obtain ⟨z, lt_xz, hz⟩ := Satisfies.dia_def.mp hx₂;
+  rcases lt_trichotomy y z with (lt_yz | rfl | lt_zy);
+  . rcases or_not_of_imp (hx₁ y lt_xy) with _ | hy;
+    . assumption;
+    . exfalso;
+      obtain ⟨⟨u, u_ioc⟩, hu₁, hu⟩ := @WellFounded.has_min (α := Finset.Ioc y z) (· > ·)
+        (Finite.wellFounded_of_trans_of_irrefl _)
+        ({ u | ¬Satisfies ⟨NatLTFrame, V⟩ u (.atom 0) })
+        (by
+          replace hy := Satisfies.box_def.not.mp hy;
+          push_neg at hy;
+          obtain ⟨u, lt_yu, hu⟩ := hy;
+          refine ⟨⟨u, ?_⟩, ?_⟩;
+          . apply Finset.mem_Ioc.mpr;
+            constructor;
+            . tauto;
+            . by_contra hC;
+              exact hu $ hz _ $ not_le.mp hC;
+          . tauto;
+        )
+      have ⟨lt_yu, le_uz⟩ := Finset.mem_Ioc.mp u_ioc;
+      have := not_imp_not.mpr (hx₁ u (lt_trans lt_xy lt_yu)) hu₁;
+      replace this := Satisfies.box_def.not.mp this;
+      push_neg at this;
+      obtain ⟨v, lt_uv, hv⟩ := this;
+      have := hu ⟨v, ?_⟩ hv;
+      . contradiction;
+      . by_contra hC;
+        apply hv;
+        apply hz;
+        replace hC := Finset.mem_Ioc.not.mp hC;
+        push_neg at hC;
+        apply hC;
+        exact lt_trans lt_yu lt_uv;
+  . apply hx₁ <;> assumption;
+  . apply hz;
+    assumption;
 
 end LO.Modal.Kripke
