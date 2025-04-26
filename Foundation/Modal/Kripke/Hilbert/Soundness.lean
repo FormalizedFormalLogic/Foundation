eb266c20b25a7b8be19a639170e4d81da89ba633
SnO2WMaN <me@sno2wman.net>
2025-04-26 20:25:27 +0900

    add(Modal/Kripke): Soundness of `KT4.3Z`

--- Foundation/Modal/Kripke/Hilbert/Soundness.lean
+++ Foundation/Modal/Kripke/Hilbert/Soundness.lean
@@ -9,7 +9,11 @@ open Formula.Kripke
 
 namespace Hilbert.Kripke
 
-variable {H : Hilbert ℕ} {Γ : Set (Formula ℕ)} {φ : Formula ℕ} {C : Kripke.FrameClass}
+variable {H : Hilbert ℕ} {Γ : Set (Formula ℕ)} {φ : Formula ℕ}
+
+section
+
+variable {C : Kripke.FrameClass}
 
 lemma soundness_of_validates_axiomInstances (hV : C.Validates H.axiomInstances)
   : H ⊢! φ → C ⊧ φ := by
@@ -48,6 +52,47 @@ lemma consistent_of_sound_frameclass
   constructor;
   . assumption;
   . simp;
+
+end
+
+
+section
+
+variable {F : Kripke.Frame}
+
+lemma soundness_of_frame_validates_axiomInstances (hV : F ⊧* H.axiomInstances)
+  : H ⊢! φ → F ⊧ φ := by
+  intro hφ;
+  induction hφ using Hilbert.Deduction.rec! with
+  | maxm h =>
+    simp only [Semantics.RealizeSet.setOf_iff, ValidOnFrame.models_iff, forall_exists_index, and_imp] at hV;
+    obtain ⟨ψ, h, ⟨s, rfl⟩⟩ := h;
+    exact hV _ ψ h s rfl;
+  | mdp ihpq ihp => exact ValidOnFrame.mdp ihpq ihp;
+  | nec ih => exact ValidOnFrame.nec ih;
+  | imply₁ => exact ValidOnFrame.imply₁;
+  | imply₂ => exact ValidOnFrame.imply₂;
+  | ec => exact ValidOnFrame.elimContra;
+
+lemma validates_axioms_of_frame_validates_axiomInstances (hV : F ⊧* H.axioms) : F ⊧* H.axiomInstances := by
+  simp only [Semantics.RealizeSet.setOf_iff, ValidOnFrame.models_iff, forall_exists_index, and_imp] at hV ⊢;
+  rintro φ ψ hψ s rfl;
+  apply ValidOnFrame.subst;
+  exact hV.realize _ hψ;
+
+instance instSound_of_frame_validates_axioms (hV : F ⊧* H.axioms) : Sound H F := ⟨fun {_} =>
+  soundness_of_frame_validates_axiomInstances (validates_axioms_of_frame_validates_axiomInstances hV)
+⟩
+
+lemma consistent_of_sound_frames
+  (F : Kripke.Frame) [sound : Sound H F]
+  : Entailment.Consistent H := by
+  apply Entailment.Consistent.of_unprovable (f := ⊥);
+  apply not_imp_not.mpr sound.sound;
+  exact Kripke.ValidOnFrame.bot_def;
+
+end
+
 
 end Hilbert.Kripke
 
