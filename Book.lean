cd4fe28436fa2dc0ed8cbfb91c5e62414ce97b00
SnO2WMaN <me@sno2wman.net>
2025-06-14 01:26:06 +0900

    Add logo

--- Book.lean
+++ Book.lean
@@ -1,9 +1,12 @@
 import Book.Main
-import VersoManual
 
 open Verso.Genre.Manual
 
-def main := manualMain (%doc Book.Main) (config := {
-  sourceLink := some "https://github.com/FormalizedFormalLogic/Foundation",
-  issueLink := some "https://github.com/FormalizedFormalLogic/Foundation/issues",
-})
+def main := manualMain (%doc Book.Main) (config := config)
+where
+  config := Config.addKaTeX {
+    extraFiles := [("static", "static")],
+    sourceLink := some "https://github.com/FormalizedFormalLogic/Foundation",
+    issueLink := some "https://github.com/FormalizedFormalLogic/Foundation/issues",
+    logo := some "/static/logo.svg",
+  }
