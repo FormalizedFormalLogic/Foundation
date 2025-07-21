2af932c66ed0846bab1130002cf027823211b787
SnO2WMaN <me@sno2wman.net>
2025-07-21 17:46:04 +0900

    fx

--- Foundation.lean
+++ Foundation.lean
@@ -144,7 +144,7 @@ import Foundation.Modal.Logic.Dz.Basic
 
 import Foundation.Modal.Maximal.Makinson
 
-import Foundation.Modal.VanBentham.Makinson
+import Foundation.Modal.VanBentham.StandardTranslation
 
 -- Provability Logic
 import Foundation.ProvabilityLogic.Incompleteness
