d0a54383c290628533338ae980785b8b58b6b292
SnO2WMaN <me@sno2wman.net>
2025-08-10 07:59:30 +0900

    fx

--- Foundation.lean
+++ Foundation.lean
@@ -149,7 +149,7 @@ import Foundation.Modal.Logic.S.Consistent
 
 import Foundation.Modal.Logic.Dz.Basic
 
-import Foundation.Modal.Logic.GLPlusBoxbot.Basic
+import Foundation.Modal.Logic.GLPlusBoxBot.Basic
 
 import Foundation.Modal.Maximal.Makinson
 
