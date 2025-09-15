401b46578557af9c68ab10cfb60f0d850dd862bd
SnO2WMaN <me@sno2wman.net>
2025-09-15 18:42:19 +0900

    `sumQuasiNormal.iff_provable_finite_provable_letterless` 2

--- Foundation/ProvabilityLogic/Classification/LetterlessTrace.lean
+++ Foundation/ProvabilityLogic/Classification/LetterlessTrace.lean
@@ -754,13 +754,7 @@ open Classical LO.Entailment in
 lemma GL.iff_provable_closed_sumQuasiNormal_subset_spectrum (hSR : X.Singular T ∨ φ.Regular T)
   : Modal.GL.sumQuasiNormal X ⊢! φ ↔ X.spectrum ⊆ φ.spectrum := by
   calc
-    _ ↔ ∃ Y, (∀ ψ ∈ Y, ψ ∈ X) ∧ Modal.GL ⊢! Finset.conj Y ➝ φ := Logic.sumQuasiNormal.iff_provable_finite_provable $ by
-      rintro Y hY s ψ;
-      suffices ∀ ξ ∈ Y, ξ⟦s⟧ = ψ → ψ ∈ X by simpa;
-      rintro ξ hξ rfl;
-      rw [Formula.subst.subst_letterless (X_letterless _ $ hY hξ)];
-      apply hY;
-      simpa;
+    _ ↔ ∃ Y, (∀ ψ ∈ Y, ψ ∈ X) ∧ Modal.GL ⊢! Finset.conj Y ➝ φ := Logic.sumQuasiNormal.iff_provable_finite_provable_letterless X_letterless
     _ ↔ ∃ Y : Finset (Formula ℕ), ∃ _ : ∀ ψ ∈ Y, ψ ∈ X, (Finset.conj Y).spectrum (Formula.letterless.of_fconj (by grind)) ⊆ φ.spectrum := by
       constructor;
       . rintro ⟨Y, _, hY₂⟩;
