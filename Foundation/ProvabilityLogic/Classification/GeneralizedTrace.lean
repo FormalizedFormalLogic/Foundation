5c479c65ad77d0b6ad82e6622abd2ad85e349c7d
SnO2WMaN <me@sno2wman.net>
2025-09-17 06:13:58 +0900

    `eq_provablityLogic_GLα_of_coinfinite_trace`

--- Foundation/ProvabilityLogic/Classification/GeneralizedTrace.lean
+++ Foundation/ProvabilityLogic/Classification/GeneralizedTrace.lean
@@ -223,7 +223,7 @@ lemma subset_GLα_of_trace_coinfinite (hL : L.trace.Coinfinite) : L ⊆ Modal.GL
         intro n h;
         apply iff_satisfies_TBB_root_ne_finHeight.mp;
         apply Satisfies.fconj_def.mp hr _;
-        simp [Tφ];
+        suffices ∃ m ∈ φ.gTrace, □^[m]⊥ = □^[n]⊥ by simpa [Tφ];
         use n;
       by_contra hC;
       apply hr _ hC rfl;
@@ -264,21 +264,6 @@ lemma subset_GLβMinus_of_trace_cofinite (hL : L.trace.Cofinite) : L ⊆ Modal.G
       apply hr M.finHeight φ hφ hC rfl;
     . rfl;
 
-lemma Logic.sumQuasiNormal.rec!_letterless
-  {motive : (φ : Formula α) → ((sumQuasiNormal L₁ L₂) ⊢! φ) → Sort}
-  (mem₁  : ∀ {φ}, (h : L₁ ⊢! φ) → motive φ (mem₁! h))
-  (mem₂  : ∀ {φ}, (h : L₂ ⊢! φ) → motive φ (mem₂! h))
-  (mdp   : ∀ {φ ψ : Formula α},
-           {hφψ : (sumQuasiNormal L₁ L₂) ⊢! φ ➝ ψ} → {hφ : (sumQuasiNormal L₁ L₂) ⊢! φ} →
-          motive (φ ➝ ψ) hφψ → motive φ hφ → motive ψ (hφψ ⨀ hφ)
-  )
-  : ∀ {φ}, (h : sumQuasiNormal L₁ L₂ ⊢! φ) → motive φ h := by
-  intro φ h;
-  induction h using Logic.sumQuasiNormal.rec! with
-  | @subst ψ s _ ihφ =>
-    sorry;
-  | _ => grind;
-
 @[simp]
 lemma Kripke.Frame.extendRoot.eq_finHeight_from_original_root {F : Kripke.Frame} {r : F} [Fintype F.World] [F.IsTree r] : Frame.World.finHeight (r : F.extendRoot 1) = F.finHeight := by
   apply finHeight_eq_iff_relItr.mpr;
@@ -297,6 +282,159 @@ lemma Kripke.Frame.extendRoot.eq_finHeight_from_original_root {F : Kripke.Frame}
         . apply HRel.Iterate.forward.mpr
           use x;
       _ = F.finHeight + 1 := by simp;
+
+
+namespace Logic
+
+variable {L L₁ L₂ : Logic α} {φ ψ : Formula α} {s : Substitution α}
+
+inductive sumQuasiNormal' (L₁ L₂ : Logic α) : Logic α
+| mem₁ {φ} (s : Substitution _) : L₁ ⊢! φ → sumQuasiNormal' L₁ L₂ (φ⟦s⟧)
+| mem₂ {φ} (s : Substitution _) : L₂ ⊢! φ → sumQuasiNormal' L₁ L₂ (φ⟦s⟧)
+| mdp {φ ψ : Formula α} : sumQuasiNormal' L₁ L₂ (φ ➝ ψ) → sumQuasiNormal' L₁ L₂ φ → sumQuasiNormal' L₁ L₂ ψ
+
+namespace sumQuasiNormal'
+
+@[grind]
+lemma mem₁! (h : L₁ ⊢! φ) : sumQuasiNormal' L₁ L₂ ⊢! (φ⟦s⟧) := by
+  apply iff_provable.mpr;
+  apply sumQuasiNormal'.mem₁ _ h;
+
+@[grind]
+lemma mem₁!_nosub (h : L₁ ⊢! φ) : sumQuasiNormal' L₁ L₂ ⊢! φ := by
+  simpa using mem₁! (s := Substitution.id) h;
+
+@[grind]
+lemma mem₂! (h : L₂ ⊢! φ) : sumQuasiNormal' L₁ L₂ ⊢! (φ⟦s⟧) := by
+  apply iff_provable.mpr;
+  apply sumQuasiNormal'.mem₂ _ h;
+
+@[grind]
+lemma mem₂!_nosub (h : L₂ ⊢! φ) : sumQuasiNormal' L₁ L₂ ⊢! φ := by
+  simpa using mem₂! (s := Substitution.id) h;
+
+instance : Entailment.ModusPonens (sumQuasiNormal' L₁ L₂) where
+  mdp := by rintro φ ψ ⟨hφψ⟩ ⟨hφ⟩; exact ⟨sumQuasiNormal'.mdp hφψ hφ⟩;
+
+lemma rec!
+  {motive : (φ : Formula α) → ((sumQuasiNormal' L₁ L₂) ⊢! φ) → Sort}
+  (mem₁  : ∀ {φ}, ∀ s, (h : L₁ ⊢! φ) → motive (φ⟦s⟧) (mem₁! h))
+  (mem₂  : ∀ {φ}, ∀ s, (h : L₂ ⊢! φ) → motive (φ⟦s⟧) (mem₂! h))
+  (mdp   : ∀ {φ ψ : Formula α},
+           {hφψ : (sumQuasiNormal' L₁ L₂) ⊢! (φ ➝ ψ)} → {hφ : (sumQuasiNormal' L₁ L₂) ⊢! φ} →
+          motive (φ ➝ ψ) hφψ → motive φ hφ → motive ψ (hφψ ⨀ hφ)
+  )
+  : ∀ {φ}, (h : sumQuasiNormal' L₁ L₂ ⊢! φ) → motive φ h := by
+  intro φ hφ;
+  induction (iff_provable.mp $ hφ) with
+  | mdp hφψ hφ ihφψ ihφ =>
+    apply mdp;
+    . apply ihφψ;
+    . apply ihφ;
+    . apply iff_provable.mpr; assumption;
+    . apply iff_provable.mpr; assumption;
+  | _ => grind;
+
+instance : (sumQuasiNormal' L₁ L₂).Substitution where
+  subst s := by
+    rintro ⟨hφ⟩;
+    constructor;
+    induction hφ with
+    | mem₁ s' h => simpa using mem₁ (s := s' ∘ s) h
+    | mem₂ s' h => simpa using mem₂ (s := s' ∘ s) h
+    | mdp _ _ ihφψ ihφ => exact mdp ihφψ ihφ
+
+end sumQuasiNormal'
+
+
+attribute [grind] Logic.sumQuasiNormal.mem₁! Logic.sumQuasiNormal.mem₂!
+
+lemma eq_sumQuasiNormal_sumQuasiNormal' : Logic.sumQuasiNormal L₁ L₂ = Logic.sumQuasiNormal' L₁ L₂ := by
+  ext φ;
+  suffices (Logic.sumQuasiNormal L₁ L₂ ⊢! φ) ↔ (Logic.sumQuasiNormal' L₁ L₂ ⊢! φ) by grind;
+  constructor;
+  . intro h;
+    induction h using Logic.sumQuasiNormal.rec! with
+    | @subst ψ s _ ihφ => exact subst! _ ihφ;
+    | mdp ihφψ ihφ => exact ihφψ ⨀ ihφ;
+    | _ => grind;
+  . intro h;
+    induction h using Logic.sumQuasiNormal'.rec! with
+    | mdp ihφψ ihφ => exact ihφψ ⨀ ihφ;
+    | _ => apply subst!; grind;
+
+@[grind]
+lemma iff_provable_sumQuasiNormal'_provable_sumQuasiNormal : (sumQuasiNormal' L₁ L₂ ⊢! φ) ↔ (sumQuasiNormal L₁ L₂ ⊢! φ) := by
+  rw [eq_sumQuasiNormal_sumQuasiNormal'];
+
+lemma sumQuasiNormal.rec!_omitSubst
+  {motive : (φ : Formula α) → ((sumQuasiNormal L₁ L₂) ⊢! φ) → Sort}
+  (mem₁  : ∀ {φ}, ∀ s, (h : L₁ ⊢! φ) → motive (φ⟦s⟧) (subst! s $ mem₁! h))
+  (mem₂  : ∀ {φ}, ∀ s, (h : L₂ ⊢! φ) → motive (φ⟦s⟧) (subst! s $ mem₂! h))
+  (mdp   : ∀ {φ ψ : Formula α},
+           {hφψ : (sumQuasiNormal L₁ L₂) ⊢! (φ ➝ ψ)} → {hφ : (sumQuasiNormal L₁ L₂) ⊢! φ} →
+           motive (φ ➝ ψ) hφψ → motive φ hφ → motive ψ (hφψ ⨀ hφ)
+  )
+  : ∀ {φ}, (h : sumQuasiNormal L₁ L₂ ⊢! φ) → motive φ h := by
+  intro φ hφ;
+  induction (iff_provable_sumQuasiNormal'_provable_sumQuasiNormal.mpr hφ) using Logic.sumQuasiNormal'.rec! with
+  | mem₁ s h => grind;
+  | mem₂ s h => grind;
+  | @mdp _ _ hφψ hφ ihφψ ihφ => exact mdp (ihφψ $ by grind) (ihφ $ by grind);
+
+attribute [grind] Logic.subst!
+
+@[grind]
+def substitution_of_letterless (L_letterless : FormulaSet.letterless L) : L.Substitution where
+  subst s := by
+    rintro ⟨hφ⟩;
+    constructor;
+    simpa [Formula.subst.subst_letterless (s := s) $ L_letterless _ hφ];
+
+lemma sumQuasiNormal.rec!_omitSubst₁ (hL₁ : L₁.Substitution)
+  {motive : (φ : Formula α) → ((sumQuasiNormal L₁ L₂) ⊢! φ) → Sort}
+  (mem₁  : ∀ {φ}, (h : L₁ ⊢! φ) → motive φ (mem₁! h))
+  (mem₂  : ∀ {φ}, ∀ s, (h : L₂ ⊢! φ) → motive (φ⟦s⟧) (subst! s $ mem₂! h))
+  (mdp   : ∀ {φ ψ : Formula α},
+           {hφψ : (sumQuasiNormal L₁ L₂) ⊢! (φ ➝ ψ)} → {hφ : (sumQuasiNormal L₁ L₂) ⊢! φ} →
+           motive (φ ➝ ψ) hφψ → motive φ hφ → motive ψ (hφψ ⨀ hφ)
+  )
+  : ∀ {φ}, (h : sumQuasiNormal L₁ L₂ ⊢! φ) → motive φ h := by
+  apply sumQuasiNormal.rec!_omitSubst;
+  . intro φ s h;
+    apply mem₁;
+    grind;
+  . assumption;
+  . assumption;
+
+lemma sumQuasiNormal.rec!_omitSubst₂ (hL₂ : L₂.Substitution)
+  {motive : (φ : Formula α) → ((sumQuasiNormal L₁ L₂) ⊢! φ) → Sort}
+  (mem₁  : ∀ {φ}, ∀ s, (h : L₁ ⊢! φ) → motive (φ⟦s⟧) (subst! s $ mem₁! h))
+  (mem₂  : ∀ {φ}, (h : L₂ ⊢! φ) → motive φ (mem₂! h))
+  (mdp   : ∀ {φ ψ : Formula α},
+           {hφψ : (sumQuasiNormal L₁ L₂) ⊢! (φ ➝ ψ)} → {hφ : (sumQuasiNormal L₁ L₂) ⊢! φ} →
+           motive (φ ➝ ψ) hφψ → motive φ hφ → motive ψ (hφψ ⨀ hφ)
+  )
+  : ∀ {φ}, (h : sumQuasiNormal L₁ L₂ ⊢! φ) → motive φ h := by
+  simp_all only [Logic.sumQuasiNormal.symm (L₁ := L₁) (L₂ := L₂)]
+  apply sumQuasiNormal.rec!_omitSubst₁ <;> assumption;
+
+lemma sumQuasiNormal.rec!_omitSubst_strong (hL₁ : L₁.Substitution) (hL₂ : L₂.Substitution)
+  {motive : (φ : Formula α) → ((sumQuasiNormal L₁ L₂) ⊢! φ) → Sort}
+  (mem₁  : ∀ {φ}, (h : L₁ ⊢! φ) → motive φ (mem₁! h))
+  (mem₂  : ∀ {φ}, (h : L₂ ⊢! φ) → motive φ (mem₂! h))
+  (mdp   : ∀ {φ ψ : Formula α},
+           {hφψ : (sumQuasiNormal L₁ L₂) ⊢! (φ ➝ ψ)} → {hφ : (sumQuasiNormal L₁ L₂) ⊢! φ} →
+           motive (φ ➝ ψ) hφψ → motive φ hφ → motive ψ (hφψ ⨀ hφ)
+  )
+  : ∀ {φ}, (h : sumQuasiNormal L₁ L₂ ⊢! φ) → motive φ h := by
+  apply sumQuasiNormal.rec!_omitSubst;
+  . intro φ h _; apply mem₁; grind;
+  . intro φ h _; apply mem₂; grind;
+  . assumption;
+
+end Logic
+
 
 end Modal
 
@@ -358,7 +496,7 @@ lemma eq_provablityLogic_GLα_of_coinfinite_trace (h : (T.ProvabilityLogic U).tr
   . intro A;
     suffices Modal.GLα (T.ProvabilityLogic U).trace ⊢! A → T.ProvabilityLogic U ⊢! A by grind;
     intro hA;
-    induction hA using Modal.Logic.sumQuasiNormal.rec! with
+    induction hA using Modal.Logic.sumQuasiNormal.rec!_omitSubst_strong (L₁ := Modal.GL) (L₂ := (T.ProvabilityLogic U).trace.image TBB) inferInstance (Logic.substitution_of_letterless (by grind)) with
     | mem₁ hA =>
       apply ProvabilityLogic.provable_iff.mpr;
       intro f;
@@ -370,8 +508,6 @@ lemma eq_provablityLogic_GLα_of_coinfinite_trace (h : (T.ProvabilityLogic U).tr
       simp_all only [Set.mem_iUnion, exists_prop];
       use A;
     | mdp ihAB ihA => exact ProvabilityLogic.mdp ihAB ihA;
-    | @subst A s ihA =>
-      sorry;
 
 lemma eq_provabilityLogic_GLβMinus_of_not_subset_S (h : ¬(T.ProvabilityLogic U) ⊆ Modal.S) : ∃ _ : (T.ProvabilityLogic U).trace.Cofinite, T.ProvabilityLogic U = Modal.GLβMinus (T.ProvabilityLogic U).trace := by
   refine ⟨?_, ?_⟩;
