7790c8ced316f6ca45e537cb771b9f1c5103aa25
SnO2WMaN <me@sno2wman.net>
2025-09-22 20:16:42 +0900

    temporary

--- Foundation/Modal/Hilbert/Axiom.lean
+++ Foundation/Modal/Hilbert/Axiom.lean
@@ -41,9 +41,93 @@ class HasD (Ax : Axiom α) where
 class HasP (Ax : Axiom α) where
   mem_P : Axioms.P ∈ Ax := by tauto;
 
+class HasB (Ax : Axiom α) where
+  p : α
+  mem_B : Axioms.B (.atom p) ∈ Ax := by tauto;
+
 class HasFour (Ax : Axiom α) where
   p : α
   mem_Four : Axioms.Four (.atom p) ∈ Ax := by tauto;
+
+class HasFourN (n : ℕ) (Ax : Axiom α) where
+  p : α
+  mem_FourN : Axioms.FourN n (.atom p) ∈ Ax := by tauto;
+
+class HasFive (Ax : Axiom α) where
+  p : α
+  mem_Five : Axioms.Five (.atom p) ∈ Ax := by tauto;
+
+class HasPoint2 (Ax : Axiom α) where
+  p : α
+  mem_Point2 : Axioms.Point2 (.atom p) ∈ Ax := by tauto;
+
+class HasWeakPoint2 (Ax : Axiom α) where
+  p : α
+  q : α
+  ne_pq : p ≠ q := by trivial;
+  mem_WeakPoint2 : Axioms.WeakPoint2 (.atom p) (.atom q) ∈ Ax := by tauto;
+
+class HasPoint3 (Ax : Axiom α) where
+  p : α
+  q : α
+  ne_pq : p ≠ q := by trivial;
+  mem_Point3 : Axioms.Point3 (.atom p) (.atom q) ∈ Ax := by tauto;
+
+class HasWeakPoint3 (Ax : Axiom α) where
+  p : α
+  q : α
+  ne_pq : p ≠ q := by trivial;
+  mem_WeakPoint3 : Axioms.WeakPoint3 (.atom p) (.atom q) ∈ Ax := by tauto;
+
+class HasPoint4 (Ax : Axiom α) where
+  p : α
+  mem_Point4 : Axioms.Point4 (.atom p) ∈ Ax := by tauto;
+
+class HasL (Ax : Axiom α) where
+  p : α
+  mem_L : Axioms.L (.atom p) ∈ Ax := by tauto;
+
+class HasZ (Ax : Axiom α) where
+  p : α
+  mem_Z : Axioms.Z (.atom p) ∈ Ax := by tauto;
+
+class HasGrz (Ax : Axiom α) where
+  p : α
+  mem_Grz : Axioms.Grz (.atom p) ∈ Ax := by tauto;
+
+class HasDum (Ax : Axiom α) where
+  p : α
+  mem_Dum : Axioms.Dum (.atom p) ∈ Ax := by tauto;
+
+class HasTc (Ax : Axiom α) where
+  p : α
+  mem_Tc : Axioms.Tc (.atom p) ∈ Ax := by tauto;
+
+class HasVer (Ax : Axiom α) where
+  p : α
+  mem_Ver : Axioms.Ver (.atom p) ∈ Ax := by tauto;
+
+class HasHen (Ax : Axiom α) where
+  p : α
+  mem_Hen : Axioms.Hen (.atom p) ∈ Ax := by tauto;
+
+class HasMcK (Ax : Axiom α) where
+  p : α
+  mem_McK : Axioms.McK (.atom p) ∈ Ax := by tauto;
+
+class HasMk (Ax : Axiom α) where
+  p : α
+  q : α
+  ne_pq : p ≠ q := by trivial;
+  mem_Mk : Axioms.Mk (.atom p) (.atom q) ∈ Ax := by tauto;
+
+class HasH1 (Ax : Axiom α) where
+  p : α
+  mem_H1 : Axioms.H (.atom p) ∈ Ax := by tauto;
+
+class HasGeach (g) (Ax : Axiom α) where
+  p : α
+  mem_Geach : Axioms.Geach g (.atom p) ∈ Ax := by tauto;
 
 end Axiom
 
