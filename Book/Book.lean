aa9fff89cf9faec0513b894864a6efc34a8c46ec
SnO2WMaN <me@sno2wman.net>
2025-10-08 16:27:53 +0900

    Font fix

--- Book/Book.lean
+++ Book/Book.lean
@@ -5,8 +5,10 @@ open Verso.Genre.Manual
 def main := manualMain (%doc Book.Main) (config := config)
 where
   config := Config.addKaTeX {
-    extraFiles := [("static", "static")],
     sourceLink := some "https://github.com/FormalizedFormalLogic/Foundation",
     issueLink := some "https://github.com/FormalizedFormalLogic/Foundation/issues",
-    logo := some "/static/logo.svg",
+    extraFiles := [("./Book/assets", "assets")],
+    logo := some "/assets/logo.svg",
+    extraCss := ["/assets/style.css"],
+    htmlDepth := 2
   }
