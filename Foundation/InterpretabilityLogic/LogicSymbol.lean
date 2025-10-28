a0b3f76740bd30147529ef4e9c55e8fbdb1c49a1
SnO2WMaN <me@sno2wman.net>
2025-10-29 00:17:45 +0900

    add(InterpretabilityLogic): Basic Setup
    Fixes #601

---
+++ Foundation/InterpretabilityLogic/LogicSymbol.lean
@@ -0,0 +1,14 @@
+import Foundation.Modal.LogicSymbol
+
+namespace LO
+
+class Rhd (F : Type*) where
+  rhd : F → F → F
+
+infixl:70 "▷" => Rhd.rhd
+
+attribute [match_pattern] Rhd.rhd
+
+class InterpretabilityLogicalConnective (F : Type*) extends BasicModalLogicalConnective F, Rhd F
+
+end LO
