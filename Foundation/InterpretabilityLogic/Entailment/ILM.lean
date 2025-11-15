05eafee22e4ee78d2fd37946c8d9fd297f6b05e8
SnO2WMaN <me@sno2wman.net>
2025-11-15 17:42:02 +0900

    M proves R

--- Foundation/InterpretabilityLogic/Entailment/ILM.lean
+++ Foundation/InterpretabilityLogic/Entailment/ILM.lean
@@ -1,4 +1,4 @@
-import Foundation.InterpretabilityLogic.Entailment.IL
+import Foundation.InterpretabilityLogic.Entailment.ILRStar
 import Foundation.InterpretabilityLogic.Entailment.ILMinus_M
 
 namespace LO.InterpretabilityLogic.Entailment
@@ -12,6 +12,21 @@ variable {S F : Type*} [DecidableEq F] [InterpretabilityLogicalConnective F] [En
 protected class ILM (𝓢 : S) extends InterpretabilityLogic.Entailment.IL 𝓢, HasAxiomM 𝓢
 
 variable [Entailment.ILM 𝓢]
+
 instance : Entailment.ILMinus_M 𝓢 where
+
+instance : Entailment.HasAxiomW 𝓢 where
+  axiomW! {φ ψ} := by
+    dsimp [Axioms.W];
+    apply C_trans $ axiomM! (χ := (∼φ));
+    apply R2!;
+    sorry;
+
+instance : Entailment.HasAxiomR 𝓢 where
+  axiomR! {φ ψ χ} := by
+    apply deduct';
+    apply rhdTrans! (of $ rhdOfLC! $ nec $ oh);
+    apply rhdTrans! (of $ axiomJ5!);
+    apply axiomM!;
 
 end LO.InterpretabilityLogic.Entailment
