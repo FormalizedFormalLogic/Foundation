1eea4712b5e9772e2f9593eb2952f717dd6b9e7b
SnO2WMaN <me@sno2wman.net>
2025-11-16 19:40:58 +0900

    ILWM₀, ILW ⪱ ILRW

--- Foundation/InterpretabilityLogic/Veltman/Logic/ILRW.lean
+++ Foundation/InterpretabilityLogic/Veltman/Logic/ILRW.lean
@@ -1,5 +1,6 @@
 import Foundation.InterpretabilityLogic.Veltman.Logic.ILP₀
 import Foundation.InterpretabilityLogic.Veltman.Logic.ILW
+import Foundation.InterpretabilityLogic.Veltman.Logic.ILWM₀
 
 namespace LO.InterpretabilityLogic
 
@@ -35,14 +36,58 @@ instance : Entailment.Consistent InterpretabilityLogic.ILRW := Veltman.consisten
 
 end ILRW
 
-instance : InterpretabilityLogic.ILR ⪱ InterpretabilityLogic.ILRW := by
+open Entailment in
+instance : InterpretabilityLogic.ILWM₀ ⪱ InterpretabilityLogic.ILRW := by
   constructor;
-  . apply weakerThan_of_subset_axioms $ by grind;
+  . apply weakerThan_of_provable_axioms;
+    intro φ hφ;
+    rcases (by simpa using hφ) with (rfl | rfl | rfl | rfl | rfl | rfl | rfl) <;> simp only [
+      axiomJ1, axiomJ2, axiomJ3, axiomJ4, axiomJ5, axiomW, axiomM₀,
+    ];
   . apply Entailment.not_weakerThan_iff.mpr;
-    use (Axioms.W (.atom 0) (.atom 1));
+    use (Axioms.R (.atom 0) (.atom 1) (.atom 2));
     constructor;
     . simp;
-    . sorry;
+    . apply Sound.not_provable_of_countermodel (𝓜 := Veltman.FrameClass.ILWM₀);
+      apply Veltman.not_validOnFrameClass_of_exists_frame;
+      let F : Veltman.Frame := {
+        toKripkeFrame := ⟨Fin 5, λ x y => (x = 0 ∧ 0 < y) ∨ (x = 1 ∧ (y = 2 ∨ y = 4)) ∨ (x = 3 ∧ y = 4)⟩
+        isGL := Modal.Kripke.Frame.isGL_of_isFiniteGL {
+          trans := by grind;
+          irrefl := by grind;
+        }
+        S w x y :=
+          (w = 0 ∧ 1 ≤ x ∧ x ≤ y) ∨
+          (w = 1 ∧ ((x = 2 ∧ y = 2) ∨ (x = 4 ∧ y = 4))) ∨
+          (w = 3 ∧ x = 4 ∧ y = 4)
+        S_cond := by grind;
+      }
+      have : F.IsILWM₀ := {
+        S_J1 := by grind;
+        S_J2 := by grind;
+        S_J4 := by grind;
+        S_J5 := by grind;
+        S_M₀ := by grind;
+        S_W {w} := by
+          apply Finite.converseWellFounded_of_trans_irrefl';
+          . infer_instance
+          . rintro x y z ⟨a, Rxa, Sway⟩ ⟨b, Ryb, Rwbz⟩;
+            use a;
+            constructor;
+            . assumption;
+            . dsimp [Frame.SRel', F] at *;
+              omega;
+          . dsimp [Irreflexive, Frame.RS, Relation.Comp];
+            push_neg;
+            grind;
+      }
+      use F;
+      constructor;
+      . apply Set.mem_setOf_eq.mpr;
+        constructor;
+      . by_contra hC;
+        have : F.SRel' 1 2 4 := Frame.HasAxiomR.of_validate_axiomR hC |>.S_R (x := 0) (u := 3) (by tauto) (by tauto) (by tauto) (by tauto);
+        simp [F, Frame.SRel'] at this;
 
 instance : InterpretabilityLogic.ILW ⪱ InterpretabilityLogic.ILRW := by
   constructor;
@@ -51,7 +96,44 @@ instance : InterpretabilityLogic.ILW ⪱ InterpretabilityLogic.ILRW := by
     use (Axioms.R (.atom 0) (.atom 1) (.atom 2));
     constructor;
     . simp;
-    . sorry;
-      -- apply Sound.not_provable_of_countermodel (𝓜 := Veltman.FrameClass.ILR);
+    . apply Sound.not_provable_of_countermodel (𝓜 := Veltman.FrameClass.ILW);
+      apply Veltman.not_validOnFrameClass_of_exists_frame;
+      let F : Veltman.Frame := {
+        toKripkeFrame := ⟨Fin 5, λ x y => (x = 0 ∧ 0 < y) ∨ (x = 1 ∧ y = 2) ∨ (x = 3 ∧ y = 4)⟩
+        isGL := Modal.Kripke.Frame.isGL_of_isFiniteGL {
+          trans := by grind;
+          irrefl := by grind;
+        }
+        S w x y :=
+          (w = 0 ∧ 1 ≤ x ∧ x ≤ y) ∨
+          (w = 1 ∧ x = 2 ∧ y = 2) ∨
+          (w = 3 ∧ x = 4 ∧ y = 4)
+        S_cond := by grind;
+      }
+      have : F.IsILW := {
+        S_J1 := by grind;
+        S_J2 {w x y z} := by grind;
+        S_J4 {w x y} := by grind;
+        S_J5 {w x y} := by grind;
+        S_W {w} := by
+          apply Finite.converseWellFounded_of_trans_irrefl';
+          . infer_instance
+          . rintro x y z ⟨a, Rxa, Sway⟩ ⟨b, Ryb, Rwbz⟩;
+            use a;
+            constructor;
+            . assumption;
+            . dsimp [Frame.SRel', F] at *;
+              omega;
+          . dsimp [Irreflexive, Frame.RS, Relation.Comp];
+            push_neg;
+            grind;
+      }
+      use F;
+      constructor;
+      . apply Set.mem_setOf_eq.mpr;
+        constructor;
+      . by_contra hC;
+        have : F.SRel' 1 2 4 := Frame.HasAxiomR.of_validate_axiomR hC |>.S_R (x := 0) (u := 3) (by tauto) (by tauto) (by tauto) (by tauto);
+        simp [F, Frame.SRel'] at this;
 
 end LO.InterpretabilityLogic
