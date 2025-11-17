ba6e580b186cb5d23c500b77cc16cfaea491ed52
palalansouki <palalansouki@gmail.com>
2025-11-17 12:33:38 +0900

    Universe

---
+++ Foundation/FirstOrder/SetTheory/TransitiveModel.lean
@@ -0,0 +1,22 @@
+import Foundation.FirstOrder.SetTheory.Basic
+import Foundation.FirstOrder.SetTheory.Universe
+
+namespace LO.FirstOrder.SetTheory
+
+abbrev TransitiveModel.{u} := Set Universe.{u}
+
+namespace TransitiveModel
+
+variable {U : TransitiveModel}
+
+lemma wellFounded : WellFounded (α := U) (· ∈ ·) := Universe.wellFounded.onFun (f := Subtype.val)
+
+@[elab_as_elim]
+theorem ind {P : U → Prop} (ind : ∀ x, (∀ y ∈ x, P y) → P x) (x : U) : P x :=
+  wellFounded.induction x ind
+
+noncomputable def model [Small U] : Universe := .mk U
+
+end TransitiveModel
+
+end LO.FirstOrder.SetTheory
