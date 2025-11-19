abfeacb7df7545fe7c677ea9598c95e1d9483e54
SnO2WMaN <me@sno2wman.net>
2025-11-19 20:59:48 +0900

    AxiomSym

---
+++ Foundation/Propositional/Kripke2/AxiomSym.lean
@@ -0,0 +1,59 @@
+import Foundation.Propositional.Kripke2.Basic
+import Foundation.Vorspiel.HRel.Convergent
+import Foundation.Vorspiel.HRel.Euclidean
+import Foundation.Vorspiel.HRel.Coreflexive
+
+namespace LO.Propositional
+
+namespace Axioms
+
+variable {F : Type*} [LogicalConnective F]
+variable (φ ψ χ : F)
+
+/-- Axioms of symmetry -/
+protected abbrev Sym := φ ➝ (ψ ⋎ ∼(φ ➝ ψ))
+
+end Axioms
+
+
+open Kripke2
+open Formula.Kripke2
+
+namespace Kripke2
+
+variable {F : Kripke2.Frame} {φ ψ : Formula ℕ}
+
+namespace Frame
+
+protected abbrev IsSymmetric (F : Kripke2.Frame) := _root_.IsSymm _ F.Rel
+@[simp, grind →] lemma symm [F.IsSymmetric] : ∀ {x y : F}, x ≺ y → y ≺ x := by apply IsSymm.symm
+
+end Frame
+
+
+@[simp high, grind .]
+lemma valid_axiomSym_of_isSymmetric [F.IsSymmetric] : F ⊧ Axioms.Sym φ ψ := by
+  intro V x y Rxy h₁;
+  apply Satisfies.def_or.mpr;
+  apply or_iff_not_imp_left.mpr;
+  intro h₂;
+  apply Satisfies.def_neg.mpr;
+  intro z Ryz;
+  apply Satisfies.not_def_imp.mpr;
+  use y;
+  refine ⟨?_, ?_, ?_⟩;
+  . apply F.symm Ryz;
+  . assumption;
+  . assumption;
+
+lemma isSymmetric_of_valid_axiomSym (h : F ⊧ Axioms.Sym #0 #1) : F.IsSymmetric := by
+  constructor;
+  intro x y Rxy;
+  rcases @h (λ w a => match a with | 0 => w = x | 1 => y ≺ w | _ => False) F.root x F.rooted (by tauto) with h | h;
+  . assumption;
+  . have := h Rxy;
+    simp [Satisfies] at this;
+
+end Kripke2
+
+end LO.Propositional
