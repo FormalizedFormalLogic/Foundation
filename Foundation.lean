87179e52ec6ab1da9559fc8074193ba2f26c8897
SnO₂WMaN <me@sno2wman.net>
2026-01-10 23:37:18 +0900

    refactor: Move Gödel-Rosser Incompleteness Theorem (#697)

--- Foundation.lean
+++ Foundation.lean
@@ -63,14 +63,15 @@ import Foundation.FirstOrder.Bootstrapping.Consistency
 import Foundation.FirstOrder.Bootstrapping.WitnessComparison
 import Foundation.FirstOrder.Bootstrapping.RosserProvability
 
-import Foundation.FirstOrder.Incompleteness.First
-import Foundation.FirstOrder.Incompleteness.Halting
 import Foundation.FirstOrder.Incompleteness.Dense
-import Foundation.FirstOrder.Incompleteness.Second
 import Foundation.FirstOrder.Incompleteness.Examples
+import Foundation.FirstOrder.Incompleteness.First
+import Foundation.FirstOrder.Incompleteness.GödelRosser
+import Foundation.FirstOrder.Incompleteness.Halting
+import Foundation.FirstOrder.Incompleteness.Löb
+import Foundation.FirstOrder.Incompleteness.Second
 import Foundation.FirstOrder.Incompleteness.Tarski
 import Foundation.FirstOrder.Incompleteness.Yablo
-import Foundation.FirstOrder.Incompleteness.Löb
 
 import Foundation.FirstOrder.SetTheory.Basic
 import Foundation.FirstOrder.SetTheory.Universe
