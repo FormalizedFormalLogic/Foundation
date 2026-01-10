4cdb3c6ba3dfd4ad8aa14644ec8431ff4d28e5f3
SnO2WMaN <me@sno2wman.net>
2026-01-10 20:20:08 +0900

    done

--- Foundation/FirstOrder/Incompleteness/RestrictedProvability.lean
+++ Foundation/FirstOrder/Incompleteness/RestrictedProvability.lean
@@ -16,10 +16,7 @@ variable {T U : Theory L} [T.Δ₁] [U.Δ₁]
 /-- Provability with restriction of proof-length -/
 def RestrictedProvable (𝔢 : ℕ) (T : Theory L) [T.Δ₁] (φ : V) := ∃ d ≤ Exp.exp (ORingStructure.numeral 𝔢), T.Proof d φ
 
-noncomputable def restrictedProvable (𝔢 : ℕ) : 𝚷₁.Semisentence 1 := .mkPi
-  “φ. ∀ E, !expDef E !𝔢 → ∃ d, d ≤ E ∧ !T.proof.sigma d φ” $ by
-    simp;
-    sorry;
+noncomputable def restrictedProvable (𝔢 : ℕ) : 𝚷₁.Semisentence 1 := .mkPi “φ. ∀ E, !expDef E !𝔢 → ∃ d <⁺ E, !T.proof.pi d φ”
 
 noncomputable abbrev restrictedProvabilityPred (𝔢 : ℕ) (σ : Sentence L) : ArithmeticSentence := (T.restrictedProvable 𝔢).val/[⌜σ⌝]
 
@@ -29,11 +26,9 @@ instance RestrictedProvable.defined {𝔢} : 𝚷₁-Predicate[V] T.RestrictedPr
 /-- Gödel sentence by restricted provability -/
 noncomputable abbrev restrictedGödel (𝔢 : ℕ) (T : Theory L) [T.Δ₁] : ArithmeticSentence := fixedpoint (∼(T.restrictedProvable 𝔢))
 
-@[simp]
-lemma restrictedGödel_sigmaOne {𝔢 : ℕ} : Hierarchy 𝚺 1 (T.restrictedGödel 𝔢) := by
-  -- dsimp [Theory.restrictedGödel, fixedpoint, diag];
-  -- apply Hierarchy.ball (Γ := 𝚺) (s := 1);
-  sorry;
+noncomputable abbrev restrictedGödel' (𝔢 : ℕ) (T : Theory L) [T.Δ₁] : ArithmeticSentence := ∼(T.restrictedProvable 𝔢)/[⌜restrictedGödel 𝔢 T⌝]
+
+lemma restrictedGödel'_sigmaOne {𝔢 : ℕ} : Hierarchy 𝚺 1 (T.restrictedGödel' 𝔢) := by definability;
 
 end Theory
 
@@ -46,6 +41,21 @@ variable {𝔢 : ℕ}
 
 lemma def_restrictedGödel [𝗜𝚺₁ ⪯ U] : U ⊢ T.restrictedGödel 𝔢 ⭤ (∼T.restrictedProvable 𝔢)/[⌜T.restrictedGödel 𝔢⌝] := diagonal _
 
+lemma def_restrictedGödel' [𝗜𝚺₁ ⪯ U] : U ⊢ T.restrictedGödel' 𝔢 ⭤ (∼T.restrictedProvable 𝔢)/[⌜T.restrictedGödel 𝔢⌝] := by simp;
+
+lemma provable_E_restrictedGödel_restrictedGödel' [𝗜𝚺₁ ⪯ U] : U ⊢ T.restrictedGödel 𝔢 ⭤ T.restrictedGödel' 𝔢 := by
+  apply Entailment.E!_trans;
+  . exact def_restrictedGödel;
+  . exact Entailment.E!_symm $ def_restrictedGödel';
+
+lemma iff_provable_restrictedGödel_provable_restrictedGödel' [𝗜𝚺₁ ⪯ U] : U ⊢ (T.restrictedGödel 𝔢) ↔ U ⊢ (T.restrictedGödel' 𝔢) := by
+  apply Entailment.iff_of_E! provable_E_restrictedGödel_restrictedGödel';
+
+lemma iff_true_restrictedGödel_true_restrictedGödel' : ℕ ⊧ₘ (T.restrictedGödel 𝔢) ↔ ℕ ⊧ₘ (T.restrictedGödel' 𝔢) := by
+  apply Semantics.models_iff.mp;
+  apply models_of_provable (T := 𝗜𝚺₁) inferInstance;
+  apply provable_E_restrictedGödel_restrictedGödel';
+
 lemma models_restrictedGödel : V ⊧ₘ T.restrictedGödel 𝔢 ↔ ∀ x : V, x ≤ Exp.exp (ORingStructure.numeral 𝔢) → ¬T.Proof x (⌜T.restrictedGödel 𝔢⌝) := by
   apply Iff.trans $ Semantics.models_iff.mp $ models_of_provable (T := 𝗜𝚺₁) inferInstance $ def_restrictedGödel;
   simp [models_iff, Theory.RestrictedProvable]
@@ -53,18 +63,25 @@ lemma models_restrictedGödel : V ⊧ₘ T.restrictedGödel 𝔢 ↔ ∀ x : V, 
 lemma models_neg_restrictedGödel : ¬V ⊧ₘ T.restrictedGödel 𝔢 ↔ ∃ x : V, x ≤ Exp.exp (ORingStructure.numeral 𝔢) ∧ T.Proof x (⌜T.restrictedGödel 𝔢⌝) := by
   simpa using models_restrictedGödel.not;
 
-theorem true_restrictedGödel (𝔢) [T.SoundOnHierarchy 𝚺 1] : ℕ ⊧ₘ (T.restrictedGödel 𝔢) := by
+theorem true_restrictedGödel [𝗜𝚺₁ ⪯ T] [T.SoundOnHierarchy 𝚺 1] : ℕ ⊧ₘ T.restrictedGödel 𝔢 := by
   by_contra hC;
   obtain ⟨e, _, he⟩ := models_neg_restrictedGödel (𝔢 := 𝔢) |>.mp hC;
   apply hC;
-  apply ArithmeticTheory.soundOnHierarchy T _ _ ?_ T.restrictedGödel_sigmaOne;
-  apply Arithmetic.Bootstrapping.provable_of_standard_proof (V := ℕ) (T := T) (n := e);
+  apply iff_true_restrictedGödel_true_restrictedGödel'.mpr;
+  apply ArithmeticTheory.soundOnHierarchy T _ _ ?_ T.restrictedGödel'_sigmaOne;
+  apply iff_provable_restrictedGödel_provable_restrictedGödel'.mp;
+  apply Arithmetic.Bootstrapping.provable_of_standard_proof (T := T) (V := ℕ) (n := e);
   simpa using he;
 
-theorem provable_restrictedGödel (𝔢) [T.SoundOnHierarchy 𝚺 1] [𝗥₀ ⪯ T] : T ⊢ T.restrictedGödel 𝔢 :=
-  Arithmetic.sigma_one_completeness_iff (by definability) |>.mp $ true_restrictedGödel 𝔢
+theorem provable_restrictedGödel [𝗥₀ ⪯ T] [𝗜𝚺₁ ⪯ T] [T.SoundOnHierarchy 𝚺 1] : T ⊢ T.restrictedGödel 𝔢 := by
+  apply iff_provable_restrictedGödel_provable_restrictedGödel'.mpr;
+  apply Arithmetic.sigma_one_completeness_iff T.restrictedGödel'_sigmaOne |>.mp;
+  apply iff_true_restrictedGödel_true_restrictedGödel'.mp $ true_restrictedGödel;
 
-example [T.SoundOnHierarchy 𝚺 1] [𝗥₀ ⪯ T] : ℕ ⊧ₘ T.restrictedGödel 100 ∧ T ⊢ T.restrictedGödel 100 := by
+/--
+  "This sentence cannot be proved by proof its Gödel number smaller than `2^100`" is true and provable.
+-/
+example [T.SoundOnHierarchy 𝚺 1] [𝗥₀ ⪯ T] [𝗜𝚺₁ ⪯ T] : ℕ ⊧ₘ T.restrictedGödel 100 ∧ T ⊢ T.restrictedGödel 100 := by
   constructor;
   . apply true_restrictedGödel;
   . apply provable_restrictedGödel;
