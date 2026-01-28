1c1b23f427e8b5bd744a27f7b71115ee35210c81
SnO2WMaN <me@sno2wman.net>
2026-01-28 12:07:11 +0900

    fix

--- Foundation/Modal/Kripke/Algebra.lean
+++ Foundation/Modal/Kripke/Algebra.lean
@@ -44,62 +44,56 @@ end Frame
 
 variable {F : Kripke.Frame} {A B : F⁺} {x : F.World} {φ ψ : Formula ℕ}
 
-def Valuation.toV (V : Valuation F) : ℕ → F⁺ := λ n x => V x n
-
 open Formula.Kripke
 
-lemma algebraic_lemma (F : Kripke.Frame) (V : Valuation F) (x : F.World) : (Satisfies ⟨F, V⟩ x φ) ↔ x ∈ ((V.toV) ⊩ φ) := by
+@[grind =]
+lemma algebraic_satisfies (F : Kripke.Frame) (V : ℕ → F⁺) (x : F.World) : (Satisfies ⟨F, V⟩ x φ) ↔ x ∈ (V ⊩ φ) := by
   induction φ generalizing x with
   | hatom a => simp [Satisfies, Formula.value]; tauto;
   | hfalsum => simp [Satisfies];
   | himp φ ψ ihφ ihψ => simp [Satisfies, ihφ x, ihψ x]; tauto;
   | hbox φ ihφ => simp [Satisfies, ihφ, Frame.mem_def_box];
 
-lemma algebraic_imp : F ⊧ φ ➝ ψ ↔ (∀ V : ℕ → F⁺, (V ⊩ φ) ≤ (V ⊩ ψ)) := by
+lemma algebraic_validOnFrame_imp : F ⊧ φ ➝ ψ ↔ (∀ V : ℕ → F⁺, (V ⊩ φ) ≤ (V ⊩ ψ)) := by
   constructor;
   . intro H V x h;
-    apply algebraic_lemma _ (λ x a => V a x) _ |>.mp;
-    apply H;
-    apply algebraic_lemma _ (λ x a => V a x) _ |>.mpr;
-    exact h;
+    have := @H V x;
+    dsimp [Semantics.Models, Satisfies] at this;
+    grind;
   . intro H V x h;
-    apply algebraic_lemma _ (λ x a => V x a) _ |>.mpr;
-    apply H;
-    apply algebraic_lemma _ (λ x a => V x a) _ |>.mp;
-    exact h;
+    have := @H V x;
+    grind;
 
-lemma algebraic_iff : F ⊧ φ ⭤ ψ ↔ (∀ V : ℕ → F⁺, (V ⊩ φ) = (V ⊩ ψ)) := by
+lemma algebraic_validOnFrame_iff : F ⊧ φ ⭤ ψ ↔ (∀ V : ℕ → F⁺, (V ⊩ φ) = (V ⊩ ψ)) := by
   constructor;
   . intro H V;
     apply le_antisymm;
-    . apply algebraic_imp.1;
+    . apply algebraic_validOnFrame_imp.1;
       intro V x;
       apply (Satisfies.iff_def.mp $ H V x).1;
-    . apply algebraic_imp.1;
+    . apply algebraic_validOnFrame_imp.1;
       intro V x;
       apply (Satisfies.iff_def.mp $ H V x).2;
   . intro H V x;
     apply Satisfies.iff_def.mpr;
     constructor;
     . intro h;
-      apply algebraic_imp (φ := φ).2;
+      apply algebraic_validOnFrame_imp (φ := φ).2;
       . grind;
       . exact h;
     . intro h;
-      apply algebraic_imp (φ := ψ).2;
+      apply algebraic_validOnFrame_imp (φ := ψ).2;
       . grind;
       . exact h;
 
-lemma algebraic : F ⊧ φ ↔ (∀ V : ℕ → F⁺, (V ⊩ φ) = ⊤) := by
-  have := algebraic_iff (F := F) (φ := φ) (ψ := ⊤);
+lemma algebraic_validOnFrame : F ⊧ φ ↔ (∀ V : ℕ → F⁺, (V ⊩ φ) = ⊤) := by
+  have := algebraic_validOnFrame_iff (F := F) (φ := φ) (ψ := ⊤);
   simp_all only [
     ValidOnFrame.models_iff, Formula.eq_value_verum, Set.top_eq_univ,
     Set.eq_univ_iff_forall
   ];
   apply Iff.trans ?_ this;
-  constructor;
-  . simp [ValidOnFrame, ValidOnModel, Semantics.Models, Satisfies];
-  . simp [ValidOnFrame, ValidOnModel, Semantics.Models, Satisfies];
+  constructor <;> simp [ValidOnFrame, ValidOnModel, Semantics.Models, Satisfies];
 
 end Kripke
 
