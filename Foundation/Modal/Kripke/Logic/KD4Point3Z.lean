463333c4afabd7f7f66028967ee16d9fdac7f30c
SnO2WMaN <me@sno2wman.net>
2026-02-17 06:13:25 +0900

    run `lake exe mk_all`

--- Foundation/Modal/Kripke/Logic/KD4Point3Z.lean
+++ Foundation/Modal/Kripke/Logic/KD4Point3Z.lean
@@ -1,10 +1,10 @@
 module
 
 public import Foundation.Modal.Kripke.AxiomL
-public import Foundation.Modal.Kripke.Logic.Soundness
 public import Foundation.Modal.Kripke.LinearFrame
 public import Foundation.Modal.Kripke.AxiomWeakPoint3
 public import Foundation.Modal.Kripke.AxiomGeach
+public import Foundation.Modal.Kripke.Logic.KD4
 
 @[expose] public section
 
@@ -18,15 +18,21 @@ open Kripke
 open Modal.Kripke
 
 instance : Sound Modal.KD4Point3Z natLT := instSound_of_frame_validates_axioms $ by
-  simp only [Semantics.ModelsSet.insert_iff, ValidOnFrame.models_iff, Semantics.ModelsSet.singleton_iff];
+  suffices
+    ValidOnFrame natLT (Axioms.K (atom 0) (atom 1)) ∧
+    ValidOnFrame natLT (Axioms.D (atom 0)) ∧
+    ValidOnFrame natLT (Axioms.Four (atom 0)) ∧
+    ValidOnFrame natLT (Axioms.WeakPoint3 (atom 0) (atom 1)) ∧
+    ValidOnFrame natLT (Axioms.Z (atom 0)) by
+    simpa [Semantics.ModelsSet.insert_iff, ValidOnFrame.models_iff, Semantics.ModelsSet.singleton_iff];
   refine ⟨?_, ?_, ?_, ?_, ?_⟩;
-  . apply FrameClass.K.validates_axiomK <;> tauto;
+  . tauto;
   . apply validate_AxiomD_of_serial;
   . apply validate_AxiomFour_of_transitive;
   . apply validate_WeakPoint3_of_weakConnected;
   . apply Kripke.natLT_validates_AxiomZ;
 
-instance : Entailment.Consistent Logic.KD4Point3Z := consistent_of_sound_frames natLT
+instance : Entailment.Consistent Modal.KD4Point3Z := consistent_of_sound_frames natLT
 
 end LO.Modal
 end
