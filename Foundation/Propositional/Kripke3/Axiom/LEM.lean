d8f192b0a31dc410072f608f86d23ac18ae23087
SnO2WMaN <me@sno2wman.net>
2026-03-11 16:22:38 +0900

    LEM

--- Foundation/Propositional/Kripke3/Axiom/LEM.lean
+++ Foundation/Propositional/Kripke3/Axiom/LEM.lean
@@ -2,6 +2,7 @@ module
 
 public import Foundation.Propositional.Kripke3.Basic
 public import Foundation.Vorspiel.Rel.Euclidean
+public import Foundation.Propositional.Kripke3.Logic.Int.Completeness
 
 @[expose] public section
 
@@ -49,6 +50,35 @@ lemma isRightEuclidean_of_validates_axiomLEM [Std.Refl K] [IsTrans _ K]
     apply Std.Refl.refl;
 
 end KripkeModel
+
+section
+
+variable {S} [Entailment S (Formula ℕ)]
+variable {𝓢 : S} [Entailment.Consistent 𝓢] [Entailment.Int 𝓢]
+
+open Formula.Kripke
+open LO.Entailment
+     LO.Entailment.FiniteContext
+open canonicalKripkeModel
+open SaturatedConsistentTableau
+open Classical
+
+instance [Entailment.HasAxiomLEM 𝓢] : IsRightEuclidean (canonicalKripkeModel 𝓢).rel := by
+  constructor;
+  intro x y z Rxy;
+  contrapose!;
+  rintro nRyz;
+  obtain ⟨φ, hφy, hφz⟩ := Set.not_subset.mp nRyz;
+  apply Set.not_subset.mpr;
+  use φ;
+  constructor;
+  . rcases (show φ ∈ x.1.1 ∨ ∼φ ∈ x.1.1 by exact iff_mem₁_or.mp $ mem₁_of_provable (by simp)) with h | h;
+    . assumption;
+    . have : φ ∉ y.1.1 := of_mem₁_neg' $ Rxy h;
+      contradiction;
+  . assumption;
+
+end
 
 end LO.Propositional
 end
