fb54a62c19fc41debc30bf85b610eb76fc67f304
palalansouki <palalansouki@gmail.com>
2026-04-04 20:29:38 +0900

    add CutFree

--- Foundation/FirstOrder/Basic.lean
+++ Foundation/FirstOrder/Basic.lean
@@ -9,6 +9,7 @@ public import Foundation.FirstOrder.Basic.Operator
 public import Foundation.FirstOrder.Basic.BinderNotation
 public import Foundation.FirstOrder.Basic.Model
 public import Foundation.FirstOrder.Basic.Calculus
+public import Foundation.FirstOrder.Basic.CutFree
 public import Foundation.FirstOrder.Basic.Calculus2
 public import Foundation.FirstOrder.Basic.Coding
 public import Foundation.FirstOrder.Basic.Eq
