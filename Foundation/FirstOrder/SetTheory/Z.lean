5150cfcf26ddc98c3f86ca4b3e63454e36100945
Convindix <gd.convindix@gmail.com>
2026-07-13 22:40:24 -0500

    Update names in ZF.lean, convert to typeclasses in Z.lean

--- Foundation/FirstOrder/SetTheory/Z.lean
+++ Foundation/FirstOrder/SetTheory/Z.lean
@@ -285,26 +285,26 @@ lemma separation_exists_eval (x : V) (φ : SetTheorySemiformula V 1) : ∃ y : V
   have := by simpa [models_iff, Semiformula.eval_univCl, Axiom.separationSchema] using Theory.models V 𝗭 (Zermelo.axiom_of_separation ψ)
   simpa [ψ, f, Semiformula.eval_rewriteMap, Matrix.constant_eq_singleton] using this f x
 
-lemma separation_exists (x : V) (P : V → Prop) (hP : ℒₛₑₜ-predicate P) : ∃ y : V, ∀ z : V, z ∈ y ↔ z ∈ x ∧ P z := by
+lemma separation_exists (x : V) (P : V → Prop) [hP : ℒₛₑₜ-predicate P] : ∃ y : V, ∀ z : V, z ∈ y ↔ z ∈ x ∧ P z := by
   rcases hP with ⟨φ, hP⟩
   simpa [hP.iff] using separation_exists_eval x φ
 
-lemma separation_existsUnique (x : V) (P : V → Prop) (hP : ℒₛₑₜ-predicate P) : ∃! y : V, ∀ z : V, z ∈ y ↔ z ∈ x ∧ P z := by
-  rcases separation_exists x P hP with ⟨s, hs⟩
+lemma separation_existsUnique (x : V) (P : V → Prop) [hP : ℒₛₑₜ-predicate P] : ∃! y : V, ∀ z : V, z ∈ y ↔ z ∈ x ∧ P z := by
+  rcases separation_exists x P with ⟨s, hs⟩
   apply ExistsUnique.intro s hs
   intro u hu
   ext; simp_all
 
-noncomputable def sep (x : V) (P : V → Prop) (hP : ℒₛₑₜ-predicate P) : V := Classical.choose! (separation_existsUnique x P hP)
+noncomputable def sep (x : V) (P : V → Prop) [hP : ℒₛₑₜ-predicate P] : V := Classical.choose! (separation_existsUnique x P)
 
 @[simp] lemma mem_sep_iff {P : V → Prop} {hP : ℒₛₑₜ-predicate P} {z x : V} :
-    z ∈ sep x P hP ↔ z ∈ x ∧ P z := Classical.choose!_spec (separation_existsUnique x P hP) z
+    z ∈ sep x P ↔ z ∈ x ∧ P z := Classical.choose!_spec (separation_existsUnique x P) z
 
-@[simp] lemma sep_empty_eq (P : V → Prop) (hP : ℒₛₑₜ-predicate P) :
-    sep ∅ P hP = ∅ := by ext; simp
+@[simp] lemma sep_empty_eq (P : V → Prop) {hP : ℒₛₑₜ-predicate P} :
+    sep ∅ P = ∅ := by ext; simp
 
 @[simp] lemma sep_subset {P : V → Prop} {hP : ℒₛₑₜ-predicate P} {x : V} :
-    sep x P hP ⊆ x := by intro z; simp; tauto
+    sep x P ⊆ x := by intro z; simp; tauto
 
 section set_notation
 
@@ -318,7 +318,7 @@ syntax (name := internalSetBuilder) "{" binderIdent " ∈ " term " ; " term "}" 
 @[term_elab internalSetBuilder]
 meta def elabInternalSetBuilder : TermElab
   | `({ $x:ident ∈ $s ; $p }), expectedType? => do
-    elabTerm (← `(sep $s (fun $x:ident ↦ $p) (by definability))) expectedType?
+    elabTerm (← `(sep $s (fun $x:ident ↦ $p) (hP := by definability))) expectedType?
   | _, _ => throwUnsupportedSyntax
 
 @[app_unexpander sep]
--- Foundation/FirstOrder/SetTheory/ZF.lean
+++ Foundation/FirstOrder/SetTheory/ZF.lean
@@ -31,7 +31,7 @@ lemma replacement_exists_eval (X : V) (φ : SetTheorySemiformula V 2) (h : (∀ 
 /--
 Replacement exists (for a relation).
 -/
-lemma replacement_exists (X : V) (R : V → V → Prop) (hR : ℒₛₑₜ-relation R) (h : ∀ x, ∃! y, R x y) :
+lemma replacement_rel_exists (X : V) (R : V → V → Prop) [hR : ℒₛₑₜ-relation R] (h : ∀ x, ∃! y, R x y) :
     ∃ Y : V, ∀ y, y ∈ Y ↔ ∃ x ∈ X, R x y := by
   rcases hR with ⟨φ, hR⟩
   -- Put hR in a useful form
@@ -39,40 +39,87 @@ lemma replacement_exists (X : V) (R : V → V → Prop) (hR : ℒₛₑₜ-relat
   have cond : ∀ x : V, ∃! y : V, φ.Eval ![x, y] id := by simpa [← hR] using h
   simpa [hR] using replacement_exists_eval X φ cond
 
-lemma replacement_existsUnique (X : V) (R : V → V → Prop) (hR : ℒₛₑₜ-relation R) (h : ∀ x, ∃! y, R x y) :
+/--
+Replacement exists uniquely (for a relation).
+-/
+lemma replacement_rel_existsUnique (X : V) (R : V → V → Prop) [hR : ℒₛₑₜ-relation R] (h : ∀ x, ∃! y, R x y) :
     ∃! Y : V, ∀ y : V, y ∈ Y ↔ ∃ x ∈ X, R x y := by
-  rcases replacement_exists X R hR h with ⟨s, hs⟩
+  rcases replacement_rel_exists X R h with ⟨s, hs⟩
   apply ExistsUnique.intro s hs
   intro u hu
   ext; simp_all
 
 /--
+Replacement exists uniquely for a function.
+-/
+lemma replacement_existsUnique (X : V) (F : V → V) [hF : ℒₛₑₜ-function₁ F] :
+    ∃! Y : V, ∀ y, y ∈ Y ↔ ∃ x ∈ X, y = F x := by
+  let R (x y : V) : Prop := Function.Graph F y x
+  have h : ∀ (x : V), ∃! y, R x y := by
+    intro x
+    simp only [Function.Graph, existsUnique_eq, R]
+  exact replacement_rel_existsUnique X R (hR := by definability) h
+
+/--
 Replacement exists for a function.
 -/
-lemma replacement_exists_function (X : V) (F : V → V) (hF : ℒₛₑₜ-function₁ F) :
-    ∃ Y : V, ∀ y, y ∈ Y ↔ ∃ x ∈ X, y = F x := by
-  let P (x y : V) : Prop := Function.Graph F y x
-  have h : ∀ (x : V), ∃! y, P x y := by
-    intro x
-    simp only [Function.Graph, existsUnique_eq, P]
-  exact replacement_exists X P (by definability) h
+lemma replacement_exists (X : V) (F : V → V) [hF : ℒₛₑₜ-function₁ F] :
+    ∃ Y : V, ∀ y, y ∈ Y ↔ ∃ x ∈ X, y = F x := (replacement_existsUnique X F).exists
 
-lemma replacement_existsUnique_function (X : V) (F : V → V) (hF : ℒₛₑₜ-function₁ F) :
-    ∃! Y : V, ∀ y, y ∈ Y ↔ ∃ x ∈ X, y = F x := by
-  let P (x y : V) : Prop := Function.Graph F y x
-  have h : ∀ (x : V), ∃! y, P x y := by
-    intro x
-    simp only [Function.Graph, existsUnique_eq, P]
-  exact replacement_existsUnique X P (by definability) h
+/--
+The axiom of replacement for a relation.
+-/
+noncomputable def replRel (X : V) (R : V → V → Prop) [hR : ℒₛₑₜ-relation R] (h : ∀ x, ∃! y, R x y) : V := Classical.choose! (replacement_rel_existsUnique X R h)
 
 /--
 The axiom of replacement.
 -/
-noncomputable def repl (X : V) (R : V → V → Prop) (hR : ℒₛₑₜ-relation R) (h : ∀ x, ∃! y, R x y) : V := Classical.choose! (replacement_existsUnique X R hR h)
+noncomputable def repl (X : V) (F : V → V) (hF : ℒₛₑₜ-function₁ F) : V := Classical.choose! (replacement_existsUnique X F)
+
+/-! ## Various lemmas -/
+
+@[simp] lemma replRel_spec {X y : V} {R : V → V → Prop} [hR : ℒₛₑₜ-relation R] {h : ∀ x, ∃! y, R x y} :
+    y ∈ replRel X R h ↔ ∃ x ∈ X, R x y := Classical.choose!_spec (replacement_rel_existsUnique X R h) y
+
+/-! ## Variants of replacement -/
 
 /--
-The axiom of replacement, for a function.
+A stronger variant of (unique existent of) replacement, which only requires uniqueness on `X`.
+The statement of this lemma is thanks to tosiaki.
 -/
-noncomputable def repl_function (X : V) (F : V → V) (hF : ℒₛₑₜ-function₁ F) : V := Classical.choose! (replacement_existsUnique_function X F hF)
+lemma replacement_rel_existsUnique_of_mem_existsUnique (X : V) (R : V → V → Prop) [hR : ℒₛₑₜ-relation R] (h : ∀ x ∈ X, ∃! y, R x y) :
+    ∃! Y : V, ∀ y, y ∈ Y ↔ ∃ x ∈ X, R x y := by
+  /- Proof sketch: Define `R' x y` to hold iff `x ∈ X` and `R x y`, or `x ∉ X` and `y = ∅`.
+  Show that `∀ x, ∃! y, R' x y` holds, by case subdivision on whether `x ∈ X` or not.
+  Obtain `Y` from replacement.
+  Then, for any `y`, we have that `y ∈ Y` iff `∃ x ∈ X, R x y`, iff `∃ x ∈ X, R' x y`.
+  -/
+  let R' (x y : V) : Prop := x ∈ X ∧ R x y ∨ x ∉ X ∧ y = ∅
+  have cond : ∀ x, ∃! y, R' x y := by
+    intro x
+    refine Classical.byCases (p := x ∈ X) ?_ ?_ <;> (intro hx; simp_all [R'])
+  obtain ⟨Y, hY⟩ := replacement_rel_exists X R' (hR := by definability) cond
+  use Y
+  aesop
+
+/--
+A stronger variant of replacement, which only requires uniqueness on `X`.
+The statement of this lemma is thanks to tosiaki.
+-/
+lemma replacement_rel_exists_of_mem_existsUnique (X : V) (R : V → V → Prop) [hR : ℒₛₑₜ-relation R] (h : ∀ x ∈ X, ∃! y, R x y) :
+    ∃ Y : V, ∀ y, y ∈ Y ↔ ∃ x ∈ X, R x y := (replacement_rel_existsUnique_of_mem_existsUnique X R h).exists
+
+/--
+The axiom of replacement, only assuming uniqueness on `X`.
+-/
+noncomputable def replRelOfMemExistsUnique (X : V) (R : V → V → Prop) [hR : ℒₛₑₜ-relation R] (h : ∀ x ∈ X, ∃! y, R x y) : V :=
+  Classical.choose! (replacement_rel_existsUnique_of_mem_existsUnique X R h)
+
+/--
+`repl_of_mem_existsUnique` agrees with `repl`.
+TODO: This is not very useful, since it already assumes uniqueness on all of `V`.
+-/
+@[simp] lemma replRelOfMemExistsUnique_spec {X y : V} {R : V → V → Prop} [hR : ℒₛₑₜ-relation R] {h : ∀ x ∈ X, ∃! y, R x y} :
+    y ∈ replRelOfMemExistsUnique X R h ↔ ∃ x ∈ X, R x y := Classical.choose!_spec (replacement_rel_existsUnique_of_mem_existsUnique X R h) y
 
 end LO.FirstOrder.SetTheory
