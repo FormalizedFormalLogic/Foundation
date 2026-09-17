20be0b9f6154ab17edfaa4b2d0014c12664ff839
SnO2WMaN <me@sno2wman.net>
2026-09-17 10:23:16 +0900

    fix(linter-migration): clean Rosser style warnings

--- Foundation/FirstOrder/Incompleteness/RosserProvability.lean
+++ Foundation/FirstOrder/Incompleteness/RosserProvability.lean
@@ -10,6 +10,9 @@ public import Foundation.FirstOrder.Arithmetic.Bootstrapping.Syntax.CraigTrick
 
 namespace FFL.FirstOrder.Arithmetic.Bootstrapping
 
+set_option linter.style.longLine false
+set_option linter.style.openClassical false
+
 open FFL.Entailment
 
 variable {V : Type*} [ORingStructure V] [V↓[ℒₒᵣ] ⊧* 𝗜𝚺₁]
@@ -146,7 +149,8 @@ end rosserProvability
 theorem incomplete_GR (T : ArithmeticTheory) [T.Δ₁] [𝗜𝚺₁ ⪯ T] [Consistent T] : Incomplete T :=
   ProvabilityAbstraction.rosser_first_incompleteness T.rosserProvability
 
-instance {T : ArithmeticTheory} [T.RE] [𝗜𝚺₁ ⪯ T] : 𝗜𝚺₁ ⪯ T.craig := WeakerThan.trans inferInstance (inferInstance : T ⪯ T.craig)
+instance {T : ArithmeticTheory} [T.RE] [𝗜𝚺₁ ⪯ T] : 𝗜𝚺₁ ⪯ T.craig :=
+  WeakerThan.trans inferInstance (inferInstance : T ⪯ T.craig)
 
 /-- Gödel-Rosser incompleteness theorem for r.e. theories -/
 theorem incomplete_GR_of_RE (T : ArithmeticTheory) [T.RE] [𝗜𝚺₁ ⪯ T] [Consistent T] : Incomplete T :=
